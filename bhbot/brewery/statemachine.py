5b484bbe0520058c14097a83f1ce5d3082ce219b
Eric ALBER <eric.alber@gmail.com>
2010-11-10 23:05:08 +0100

    Reverted the state machine to a simple non-hierarchical fsm (KISS) Wrote some state machine tests

--- bhbot/brewery/statemachine.py
+++ bhbot/brewery/statemachine.py
@@ -6,13 +6,9 @@
 
 class StateMachine(object):
 
-    def __init__(self, start_state_ctor, owner_state = None):
-        self.current_state = start_state_ctor()
+    def __init__(self, start_state_ctor):
+        self.current_state = start_state_ctor(self)
         self.current_state.on_enter()
-        if owner_state != None:
-            self.parent_fsm = owner_state.fsm
-        else:
-            self.parent_fsm = None
 
 
 
@@ -21,22 +17,12 @@ class State(object):
 
     def __init__(self, fsm):
         self.fsm = fsm
-        self.sub_fsm = None
 
 
     def switch_to_state(self, new_state_ctor):
         self.fsm.current_state.on_exit()
-        self.fsm.current_state = new_state_ctor()
+        self.fsm.current_state = new_state_ctor(self.fsm)
         self.fsm.current_state.on_enter()
-
-
-    def exit_fsm(self):
-        self.fsm.current_state.on_exit()
-        if self.fsm.parent_fsm != None:
-            my_fsm = self.fsm.parent_fsm.current_state.sub_fsm
-            self.fsm.parent_fsm.on_sub_fsm_exited()
-            if self.fsm.parent_fsm.current_state.sub_fsm == my_fsm:
-                self.fsm.parent_fsm.current_state.sub_fsm = None
 
 
     def on_enter(self):
@@ -47,46 +33,33 @@ class State(object):
         pass
 
 
-    def on_sub_fsm_exited(self):
+    def on_device_busy(self):
         pass
 
 
-    def on_device_busy(self):
-        if self.sub_fsm != None:
-            self.sub_fsm.current_state.on_device_busy()
-
-
     def on_device_ready(self, team):
-        if self.sub_fsm != None:
-            self.sub_fsm.current_state.on_device_ready(team)
+        pass
 
 
     def on_start(self, team):
-        if self.sub_fsm != None:
-            self.sub_fsm.current_state.on_start(team)
+        pass
 
 
     def on_goto_started(self):
-        if self.sub_fsm != None:
-            self.sub_fsm.current_state.on_goto_started()
+        pass
 
 
     def on_goto_finished(self, reason):
-        if self.sub_fsm != None:
-            self.sub_fsm.current_state.on_goto_finished(reason)
+        pass
 
 
     def on_blocked(self, side):
-        if self.sub_fsm != None:
-            self.sub_fsm.current_state.on_blocked(side)
+        pass
 
 
     def on_keep_alive(self, match_started, match_time):
-        if self.sub_fsm != None:
-            self.sub_fsm.current_state.on_keep_alive(match_started, match_time)
+        pass
 
 
     def on_turret_detect(self, mean_angle, angular_size):
-        if self.sub_fsm != None:
-            self.sub_fsm.current_state.on_turret_detect(mean_angle, angular_size)
-
+        pass
--- bhbot/tests/test_statemachine.py
+++ bhbot/tests/test_statemachine.py
@@ -3,15 +3,75 @@
 
 import unittest
 
+import statemachine
+
+from definitions import *
+
+
+
+
+class TestStateMachine(statemachine.StateMachine):
+
+    def __init__(self):
+        statemachine.StateMachine.__init__(self, TestStartState)
+        self.global_data = "global data"
+
+
+
+
+
+class TestStartState(statemachine.State):
+
+    def __init__(self, fsm):
+        statemachine.State.__init__(self, fsm)
+        self.state_data = None
+
+    def on_enter(self):
+        self.state_data = "on enter"
+
+
+    def on_device_ready(self, team):
+        self.state_data = "device ready"
+
+
+    def on_start(self, team):
+        self.switch_to_state(TestEndState)
+
+
+
+
+
+class TestEndState(statemachine.State):
+
+    def __init__(self, fsm):
+        statemachine.State.__init__(self, fsm)
+
+
+    def on_enter(self):
+        self.fsm.global_data = "finished"
+
+
+
 
 
 class StateMachineTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.i = 1
+        self.fsm = TestStateMachine()
 
-    def test_is_one(self):
-        self.assertTrue(self.i == 1)
+    def test_init(self):
+        self.assertTrue(isinstance(self.fsm.current_state, TestStartState))
+        self.assertEqual(self.fsm.global_data, "global data")
+        self.assertEqual(self.fsm.current_state.state_data, "on enter")
 
-    def test_two(self):
-        self.assertEqual(self.i + 1, 2)
+    def test_first_event(self):
+        self.fsm.current_state.on_device_ready(TEAM_RED)
+        self.assertEqual(self.fsm.current_state.state_data, "device ready")
+
+    def test_transition(self):
+        self.fsm.current_state.on_device_ready(TEAM_RED)
+        self.fsm.current_state.on_start(TEAM_RED)
+        self.assertTrue(isinstance(self.fsm.current_state, TestEndState))
+        self.assertEqual(self.fsm.global_data, "finished")
+
+
