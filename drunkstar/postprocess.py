c235da22b9b47ef5ee502d89c92d0ecb6dd1b54c
Eric ALBER <eric.alber@gmail.com>
2010-11-18 00:14:13 +0100

    drunkstar: Update build scripts

--- drunkstar/postprocess.py
+++ drunkstar/postprocess.py
@@ -3,9 +3,22 @@
 
 import os
 import sys
+import shutil
 from subprocess import *
 
+def copy_skeleton():
+    skeleton = os.path.join(os.path.dirname(os.getcwd()), "skeleton")
+    for item in os.listdir(skeleton):
+        call(["cp", "-rf", os.path.join(skeleton, item), sys.argv[1]])
+ 
+def copy_kernel():
+    boot = os.path.join(sys.argv[1], "boot")
+    if not os.path.exists(boot):
+        os.mkdir(boot)
+    kernel = os.path.join(os.path.dirname(sys.argv[1]), "images", "uImage")
+    shutil.copy2(kernel, boot)
+
 if __name__ == "__main__":
-    skeleton = os.path.join(os.path.dirname(os.getcwd()), "skeleton", "*")
-    call(["cp", "-rf", skeleton, sys.argv[1]])
+    copy_skeleton()
+    copy_kernel()
     sys.exit(0)
