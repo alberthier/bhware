2a05b51ba6bf22e404207e3291d423a98515199e
Eric ALBER <eric.alber@gmail.com>
2010-11-19 13:00:06 +0100

    drunkstar: Fix image file generation (add the kernel)

--- drunkstar/build.py
+++ drunkstar/build.py
@@ -3,11 +3,13 @@
 
 import os
 import sys
+import shutil
 from subprocess import *
 
 
 BUILDROOT_VERSION="2010.08"
 BUILDROOT="buildroot-{0}".format(BUILDROOT_VERSION)
+OUTPUT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), BUILDROOT, "output")
 
 
 def download():
@@ -16,11 +18,33 @@ def download():
         if not os.path.exists(archive):
             call(["wget", "http://buildroot.uclibc.org/downloads/{0}".format(archive)])
         call(["tar", "xjf", archive])
-        call(["ln", "-s", "../{0}.config".format(BUILDROOT), "{0}/.config".format(BUILDROOT)])
 
 
 def build():
     os.chdir(BUILDROOT)
+
+    # Make without creating the rootfs
+    config_src_filepath = "../{0}.config".format(BUILDROOT)
+    config_src = file(config_src_filepath, "r")
+    config_dst = file(".config", "w")
+
+    for line in config_src:
+        if not line.startswith("BR2_TARGET_ROOTFS_"):
+            config_dst.write(line)
+
+    config_src.close()
+    config_dst.close()
+    
+    call(["make"])
+
+    # Copy the kernel in the rootfs
+    boot = os.path.join(OUTPUT_DIR, "target", "boot")
+    if not os.path.exists(boot):
+        os.mkdir(boot)
+    shutil.copy(os.path.join(OUTPUT_DIR, "images", "uImage"), boot)
+
+    # Make with the creation of the rootfs
+    shutil.copy(config_src_filepath, ".config")
     call(["make"])
 
 
--- drunkstar/postprocess.py
+++ drunkstar/postprocess.py
@@ -3,23 +3,15 @@
 
 import os
 import sys
-import shutil
 from subprocess import *
+
 
 def copy_skeleton():
     skeleton = os.path.join(os.path.dirname(os.getcwd()), "skeleton")
     for item in os.listdir(skeleton):
         call(["cp", "-rf", os.path.join(skeleton, item), sys.argv[1]])
  
-def copy_kernel():
-    boot = os.path.join(sys.argv[1], "boot")
-    if not os.path.exists(boot):
-        os.mkdir(boot)
-    kernel = os.path.join(os.path.dirname(sys.argv[1]), "images", "uImage")
-    if os.path.exists(kernel):
-        shutil.copy2(kernel, boot)
 
 if __name__ == "__main__":
     copy_skeleton()
-    copy_kernel()
     sys.exit(0)
