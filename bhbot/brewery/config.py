ebe10acec11ad9ea0d4b7e59f22c8e01782f2849
Yannick Jost <yannick@yjost.com>
2010-12-16 19:44:12 +0100

    Update configuration according to 2011 hardware

--- bhbot/brewery/config.py
+++ bhbot/brewery/config.py
@@ -5,12 +5,10 @@ import socket
 
 from definitions import *
 
-
-
-
 if socket.gethostname() == "drunkstar":
     host_device = HOST_DEVICE_ARM
-    remote_ip = "192.168.0.201"
+    ## warning PIC IP address has changed since last year
+    remote_ip = "192.168.1.200"
     serial_port_path = "/dev/ttyUSB0"
     orange_led_device_path = "/sys/class/leds/dockstar:orange:health/brightness"
     green_led_device_path = "/sys/class/leds/dockstar:green:health/brightness"
@@ -21,6 +19,12 @@ else:
     orange_led_device_path = None
     green_led_device_path = None
 
-remote_port = 2000
+## ## warning PIC remote port has changed since last year
+remote_port = 7001
 state_machine = "default"
+## should all log messages appear in the console ?
+force_log_to_screen = False
+# force_log_to_screen = True
 
+# serial port communication is disabled for the moment
+serial_port_path = None
