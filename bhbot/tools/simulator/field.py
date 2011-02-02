2eb307dee46fe0a09ae44a5d4e96d28cf456d7cc
Eric ALBER <eric.alber@gmail.com>
2011-02-02 23:39:33 +0100

    simulator: remove unused variable

--- bhbot/tools/simulator/field.py
+++ bhbot/tools/simulator/field.py
@@ -37,7 +37,6 @@ class FieldScene(QGraphicsScene):
 
     def __init__(self):
         QGraphicsScene.__init__(self)
-        self.scene = self
 
         gradiant = QLinearGradient(0.5, 0.1, 0.5, 0.9)
         gradiant.setCoordinateMode(QGradient.ObjectBoundingMode)
