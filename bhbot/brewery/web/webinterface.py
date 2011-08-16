dddc748a27d1a74c98c82b6f50ddf2fbaa1c01d2
Eric ALBER <eric.alber@gmail.com>
2011-08-17 00:23:04 +0200

    brewery: Minimal usable web interface

--- bhbot/brewery/web/webinterface.py
+++ bhbot/brewery/web/webinterface.py
@@ -4,6 +4,7 @@
 
 import sys
 import os
+import socket
 
 sys.path.append(os.path.dirname(__file__))
 
@@ -19,10 +20,58 @@ class BHWeb(object):
 
     @cherrypy.expose
     def index(self):
-        return "Hello World!\nCurrent state: {0}".format(type(self.eventloop.get_current_state()).__name__)
+        html = """<!DOCTYPE html>
+<html>
+<head>
+  <title>BH Team robot web interface</title>
+  <link rel="stylesheet" type="text/css" href="bhweb.css" />
+</head>
+<body>
+  <div class="header">
+    <h1>BH Team</h1>
+    <ul>
+      <li><a href="statemachine" target="linktarget">State Machine</a></li>
+      <li><a href="http://{0}:42080" target="linktarget">PIC</a></li>
+    </ul>
+  </div>
+  <iframe src="statemachine" name="linktarget" />
+</body>
+</html>
+""".format(socket.gethostname())
+        return html
+
+
+    @cherrypy.expose
+    def statemachine(self):
+        html = """<!DOCTYPE html>
+<html>
+<head>
+  <title>BH Team robot web interface</title>
+  <link rel="stylesheet" type="text/css" href="bhweb.css" />
+</head>
+<body>
+  <div>
+    <h2>State stack:</h2>
+    <ul>
+"""
+        state = self.eventloop.get_current_state()
+        while state != self.eventloop.root_state:
+            html += """<li>{0}</li>""".format(type(state).__name__)
+            state = state.parent_state
+        html + """</ul>
+  </div>
+  <iframe name="linktarget" />
+</body>
+</html>
+"""
+        return html
+
 
 
 
 
 def create_app(eventloop):
-    return cherrypy.tree.mount(BHWeb(eventloop), script_name = "/")
+    return cherrypy.tree.mount(BHWeb(eventloop), script_name = "/", config = { "/" : {
+        "tools.staticdir.on"  : True,
+        "tools.staticdir.dir" : os.path.join(os.path.dirname(__file__), "static"),
+    } })
