20e238ea586d934f4df67ddd6c2d4e7f74714f24
Eric ALBER <eric.alber@gmail.com>
2011-11-16 23:42:20 +0100

    brewery / tests: move tools.py testcases to tests

--- bhbot/brewery/tools.py
+++ bhbot/brewery/tools.py
@@ -1,29 +1,15 @@
-# various tools
+#!/usr/bin/env python
+# encoding: utf-8
 
 import math
-import logger
-
-epsilon = 1e-6
-
-def quasi_equal(f1,f2) :
-	return abs(f1-f2)<epsilon
-
-def quasi_null(f1) :
-	# ret = -epsilon<f1<epsilon
-	# logger.log("quasi_null {0}=={1}".format(f1,ret))
-	return -epsilon<f1<epsilon
-
-import unittest
-
-class Tests(unittest.TestCase):
-	def test_quasi_equal(self):
-		self.assertTrue(quasi_equal(1.00000001,1.0))
-		self.assertFalse(quasi_equal(1.00000001,4.0))
-
-	def test_quasi_null(self):
-		self.assertTrue(quasi_null(0.0))
-		self.assertTrue(quasi_null(0.0000001))
 
 
-if __name__ == "__main__":
-	unittest.main()
+EPSILON = 1e-6
+
+
+def quasi_equal(f1, f2):
+    return abs(f1 - f2) < EPSILON
+
+
+def quasi_null(f1):
+    return quasi_equal(f1, 0.0)
---
+++ bhbot/tests/test_tools.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import unittest
+
+import tools
+
+
+########################################################################
+
+
+class Tests(unittest.TestCase):
+
+    def test_quasi_equal(self):
+        self.assertTrue(tools.quasi_equal(1.00000001, 1.0))
+        self.assertFalse(tools.quasi_equal(1.00000001, 4.0))
+        self.assertTrue(tools.quasi_equal(tools.EPSILON, tools.EPSILON + tools.EPSILON / 2.0))
+        self.assertFalse(tools.quasi_equal(tools.EPSILON, tools.EPSILON * 2.0))
+
+
+    def test_quasi_null(self):
+        self.assertTrue(tools.quasi_null(0.0))
+        self.assertTrue(tools.quasi_null(0.0000001))
+        self.assertFalse(tools.quasi_null(1.0))
+        self.assertTrue(tools.quasi_null(tools.EPSILON / 2.0))
+        self.assertFalse(tools.quasi_null(tools.EPSILON))
+
