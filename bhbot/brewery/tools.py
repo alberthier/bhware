4c4d345c21fb2b8184a01e33a05827903268c52d
Eric ALBER <eric.alber@gmail.com>
2011-12-24 00:28:24 +0100

    brewery: add helpers

--- bhbot/brewery/tools.py
+++ bhbot/brewery/tools.py
@@ -13,3 +13,15 @@ def quasi_equal(f1, f2):
 
 def quasi_null(f1):
     return quasi_equal(f1, 0.0)
+
+
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+
+def manathan_distance(x1, y1, x2, y2):
+    return abs(x2 - x1) + abs(y2 - y1)
+
+
+def normalize_angle(a):
+    return a % (math.pi * 2.0)
