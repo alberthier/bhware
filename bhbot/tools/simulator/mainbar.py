f11ee99f078ee6d7011cffa388a38647efcc1fe0
Eric ALBER <eric.alber@gmail.com>
2012-01-13 01:04:46 +0100

    simulator: improve UI

--- bhbot/tools/simulator/mainbar.py
+++ bhbot/tools/simulator/mainbar.py
@@ -3,8 +3,12 @@
 
 import os
 
+from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4 import uic
+
+from definitions import *
+
 
 (MainBar_Ui, MainBar_Widget) =  uic.loadUiType(os.path.join(os.path.dirname(__file__), "mainbar.ui"))
 
@@ -20,8 +24,17 @@ class MainBar(QWidget, MainBar_Ui):
         self.set_icon(self.reload, "refresh")
         self.set_icon(self.start_pause, "start")
         self.set_icon(self.stop, "stop")
+        self.set_color_icon(self.purple_robot, TEAM_COLOR_PURPLE)
+        self.set_color_icon(self.red_robot, TEAM_COLOR_RED)
 
 
     def set_icon(self, button, icon_name):
         icons_dir = os.path.join(os.path.dirname(__file__), "icons")
         button.setIcon(QIcon(os.path.join(icons_dir, "{}.svg".format(icon_name))))
+
+
+    def set_color_icon(self, button, color):
+        pixmap = QPixmap(QSize(16, 16))
+        pixmap.fill(QColor(color))
+        button.setIcon(QIcon(pixmap))
+
