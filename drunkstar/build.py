7a4979f2ee083ad805e37830239518895120b7bf
Eric ALBER <eric.alber@gmail.com>
2012-02-29 02:07:39 +0100

    drunkstar: Update buildroot 2012.02 config
    --HG--
    rename : drunkstar/buildroot-2010.08-fix-microperl-compilation.patch => drunkstar/buildroot-2012.02-fix-microperl-compilation.patch

--- drunkstar/build.py
+++ drunkstar/build.py
@@ -7,64 +7,55 @@ import shutil
 from subprocess import *
 
 
-PYTHON_ARCHIVE="Python-2.7.tar.bz2"
-PYTHON_URL="http://python.org/ftp/python/2.7/{0}".format(PYTHON_ARCHIVE)
-PYSERIAL_ARCHIVE="pyserial-2.5.tar.gz"
-PYSERIAL_URL="http://pypi.python.org/packages/source/p/pyserial/{0}".format(PYSERIAL_ARCHIVE)
-MERCURIAL_ARCHIVE = "mercurial-1.7.1.tar.gz"
-MERCURIAL_URL = "http://mercurial.selenic.com/release/{0}".format(MERCURIAL_ARCHIVE)
-BUILDROOT_VERSION = "2010.08"
-BUILDROOT = "buildroot-{0}".format(BUILDROOT_VERSION)
-OUTPUT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), BUILDROOT, "output")
+MERCURIAL_VERSION = "2.1"
+MERCURIAL_ARCHIVE = "mercurial-{}.tar.gz".format(MERCURIAL_VERSION)
+MERCURIAL_URL     = "http://mercurial.selenic.com/release/{}".format(MERCURIAL_ARCHIVE)
+BUILDROOT_VERSION = "2012.02-rc3"
+BUILDROOT         = "buildroot-{}".format(BUILDROOT_VERSION)
+BUILDROOT_ARCHIVE = "{}.tar.bz2".format(BUILDROOT)
+BUILDROOT_URL     = "http://buildroot.uclibc.org/downloads/{}".format(BUILDROOT_ARCHIVE)
+
+PACKAGES = [
+             (MERCURIAL_ARCHIVE, MERCURIAL_URL),
+             (BUILDROOT_ARCHIVE, BUILDROOT_URL)
+           ]
+
+OUTPUT_DIR        = os.path.join(os.path.dirname(os.path.realpath(__file__)), BUILDROOT, "output")
 
 
 def download():
-    if not os.path.exists("{0}/Makefile".format(BUILDROOT)):
-        archive = "{0}.tar.bz2".format(BUILDROOT)
-        if not os.path.exists(archive):
-            call(["wget", "http://buildroot.uclibc.org/downloads/{0}".format(archive)])
-        call(["tar", "xjf", archive])
-        call(["patch", "-d", BUILDROOT, "-p1", "-i", "../buildroot-2010.08-target-gcc.patch"])
-        call(["patch", "-d", BUILDROOT, "-p1", "-i", "../buildroot-2010.08-dropbear-no-host-lookup.patch"])
-        call(["patch", "-d", BUILDROOT, "-p1", "-i", "../buildroot-2010.08-fix-microperl-compilation.patch"])
+    for (archive_name, url) in PACKAGES:
+        archive_file = "dl/{}".format(archive_name)
+        if not os.path.exists(archive_file):
+            call(["wget", url, "-O", archive_file])
 
-
-def install_package(archive_url, archive):
-    package = os.path.join(OUTPUT_DIR, "target", "root", archive)
-    if not os.path.exists(package):
-        call(["wget", archive_url, "-O", package])
+    if not os.path.exists("{}/Makefile".format(BUILDROOT)):
+        archive_file = "dl/{}".format(BUILDROOT_ARCHIVE)
+        call(["tar", "xjf", archive_file])
+        call(["patch", "-d", BUILDROOT, "-p1", "-i", "../buildroot-2012.02-fix-microperl-compilation.patch"])
 
 
 def build():
+    # Cleanup
+    #for (archive_name, url) in PACKAGES:
+        #archive_file = "{}/{}".format(OUTPUT_DIR, archive_name)
+        #if os.path.exists(archive_file):
+            #os.remove(archive_file)
+    #ubi_target = "{}/target/{}".format(OUTPUT_DIR, "rootfs.ubifs")
+    #if os.path.exists(ubi_target):
+        #os.remove(ubi_target)
+
+    # Build
     os.chdir(BUILDROOT)
-
-    # Make without creating the rootfs
-    config_src_filepath = "../{0}.config".format(BUILDROOT)
-    config_src = file(config_src_filepath, "r")
-    config_dst = file(".config", "w")
-
-    for line in config_src:
-        if not line.startswith("BR2_TARGET_ROOTFS_"):
-            config_dst.write(line)
-
-    config_src.close()
-    config_dst.close()
-    
+    if not os.path.exists(".config"):
+        shutil.copyfile("../{0}.config".format(BUILDROOT), ".config")
     call(["make"])
 
-    # Copy the kernel in the rootfs
-    boot = os.path.join(OUTPUT_DIR, "target", "boot")
-    if not os.path.exists(boot):
-        os.mkdir(boot)
-    shutil.copy(os.path.join(OUTPUT_DIR, "images", "uImage"), boot)
+    #ubi_image = "{}/images/{}".format(OUTPUT_DIR, "rootfs.ubifs")
+    #shutil.copyfile(ubi_image, ubi_target)
+    #call(["make"])
 
-    install_package(PYTHON_URL, PYTHON_ARCHIVE)
-    install_package(PYSERIAL_URL, PYSERIAL_ARCHIVE)
-    install_package(MERCURIAL_URL, MERCURIAL_ARCHIVE)
 
-    # Make with the creation of the rootfs
-    shutil.copy(config_src_filepath, ".config")
-    call(["make"])
 
 
 if __name__ == "__main__":
@@ -72,3 +63,4 @@ if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__))
     download()
     build()
+
