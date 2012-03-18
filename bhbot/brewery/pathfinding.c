9b16e77064489db6338e9ce531e41d1e45c74c2a
Eric ALBER <eric.alber@gmail.com>
2012-03-18 01:59:29 +0100

    brewery: Implement a-star path finding in a C module to speed up routing

---
+++ bhbot/brewery/pathfinding.c
@@ -0,0 +1,502 @@
+#include <Python.h>
+#include <structmember.h>
+
+
+typedef struct _Cell
+{
+    int x;
+    int y;
+    float h_score;
+    float g_score;
+    float f_score;
+    struct _Cell* came_from;
+    struct _Cell* next;
+} Cell;
+
+
+typedef int (*LessFunction)(Cell*, Cell*);
+
+
+Cell* cell_init(Cell* self)
+{
+    self->h_score = 0.0;
+    self->g_score = 0.0;
+    self->f_score = 0.0;
+    self->came_from = NULL;
+    self->next = NULL;
+
+    return self;
+}
+
+
+int cell_f_score_less(Cell* a, Cell* b)
+{
+    return a->f_score < b->f_score;
+}
+
+
+int cell_pointer_less(Cell* a, Cell* b)
+{
+    return a < b;
+}
+
+
+int cell_contains(Cell* self, Cell* other)
+{
+    Cell* current = NULL;
+
+    for (current = self; current != NULL; current = current->next) {
+        if (current == other) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+
+int cell_contains_sorted(Cell* self, Cell* other, LessFunction less)
+{
+    Cell* current = NULL;
+
+    for (current = self; current != NULL; current = current->next) {
+        if (current == other) {
+            return 1;
+        }
+        if (!less(current, other)) {
+            return 0;
+        }
+    }
+
+    return 0;
+}
+
+
+Cell* cell_insert_sorted(Cell* self, Cell* other, LessFunction less)
+{
+    Cell* previous = NULL;
+    Cell* current = NULL;
+
+    /* The list is empty */
+    if (self == NULL) {
+        other->next = NULL;
+        return other;
+    }
+
+    /* Middle of the list */
+    for (current = self; current != NULL; current = current->next) {
+        if (!less(current, other)) {
+            if (previous != NULL) {
+                previous->next = other;
+                other->next = current;
+                return self;
+            } else {
+                other->next = self;
+                return other;
+            }
+        }
+        previous = current;
+    }
+
+    /* other is greater than all list items */
+    previous->next = other;
+    other->next = NULL;
+    return self;
+}
+
+
+typedef struct _Wall
+{
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+    struct _Wall* next;
+} Wall;
+
+
+/********************/
+/* Pathfinder class */
+/********************/
+
+
+typedef struct
+{
+    PyObject_HEAD
+    int map_max_y;
+    int map_max_x;
+    float effective_vs_heuristic_tradeoff;
+    int main_opponent_avoidance_cells;
+    int secondary_opponent_avoidance_cells;
+    int main_opponent_x;
+    int main_opponent_y;
+    int secondary_opponent_x;
+    int secondary_opponent_y;
+    float** distance_map;
+    Cell** map;
+    Wall* walls;
+
+} PathFinder;
+
+
+static void pathfinder_dealloc(PathFinder* self)
+{
+    int x = 0;
+
+    for (x = 0; x < self->map_max_x; ++x) {
+        free(self->distance_map[x]);
+        free(self->map[x]);
+    }
+    free(self->distance_map);
+    free(self->map);
+
+    Wall* wall = self->walls;
+    while (wall != NULL) {
+        Wall* tmp = wall->next;
+        free(wall);
+        wall = tmp;
+    }
+
+    self->ob_type->tp_free((PyObject*) self);
+}
+
+
+static int pathfinder_init(PathFinder* self, PyObject* args, PyObject* kwds)
+{
+    int x = 0;
+    int y = 0;
+
+    if (!PyArg_ParseTuple(args, "iifii",
+                          &self->map_max_x,
+                          &self->map_max_y,
+                          &self->effective_vs_heuristic_tradeoff,
+                          &self->main_opponent_avoidance_cells,
+                          &self->secondary_opponent_avoidance_cells)) {
+        return -1;
+    }
+
+    self->main_opponent_x = -1;
+    self->main_opponent_y = -1;
+    self->secondary_opponent_x = -1;
+    self->secondary_opponent_y = -1;
+
+    /* Setup the map and the distance cache */
+    self->distance_map = (float**) malloc(sizeof(float*) * self->map_max_x);
+    self->map = (Cell**) malloc(sizeof(Cell*) * self->map_max_x);
+    for (x = 0; x < self->map_max_x; ++x) {
+        float* distance_row = (float*) malloc(sizeof(float) * self->map_max_y);
+        Cell* row = (Cell*) malloc(sizeof(Cell) * self->map_max_y);
+        float x_squared = x * x;
+        self->distance_map[x] = distance_row;
+        self->map[x] = row;
+        for (y = 0; y < self->map_max_y; ++y) {
+            distance_row[y] = sqrt(x_squared + y * y);
+            row[y].x = x;
+            row[y].y = y;
+        }
+    }
+
+    self->walls = NULL;
+
+    return 0;
+}
+
+
+Cell** pathfinder_append_if_valid(PathFinder* self, Cell** it, int x, int y)
+{
+    Wall* wall = NULL;
+    if (x < 0 || x >= self->map_max_x) {
+        return it;
+    }
+    if (y < 0 || y >= self->map_max_y) {
+        return it;
+    }
+    if (self->main_opponent_x != -1 && self->distance_map[abs(x - self->main_opponent_x)][abs(y - self->main_opponent_y)] < self->main_opponent_avoidance_cells) {
+        return it;
+    }
+    if (self->secondary_opponent_x != -1 && self->distance_map[abs(x - self->secondary_opponent_x)][abs(y - self->secondary_opponent_y)] < self->secondary_opponent_avoidance_cells) {
+        return it;
+    }
+    for (wall = self->walls; wall != NULL; wall = wall->next) {
+        if (x >= wall->x1 && x <= wall->x2 && y >= wall->y1 && y <= wall->y2) {
+            return it;
+        }
+    }
+    *it = &self->map[x][y];
+    ++it;
+    return it;
+}
+
+
+static void pathfinder_find_neighbor_nodes(PathFinder* self, Cell* node, Cell** neighbor_nodes)
+{
+    Cell** it = neighbor_nodes;
+    int prev_x = node->x - 1;
+    int next_x = node->x + 1;
+    int prev_y = node->y - 1;
+    int next_y = node->y + 1;
+
+    if (prev_x >= 0) {
+        if (prev_y >= 0) {
+            it = pathfinder_append_if_valid(self, it, prev_x, prev_y);
+        }
+        it = pathfinder_append_if_valid(self, it, prev_x, node->y);
+        if (next_y < self->map_max_y) {
+            it = pathfinder_append_if_valid(self, it, prev_x, next_y);
+        }
+    }
+    if (prev_y >= 0) {
+        it = pathfinder_append_if_valid(self, it, node->x, prev_y);
+    }
+    if (next_y < self->map_max_y) {
+        it = pathfinder_append_if_valid(self, it, node->x, next_y);
+    }
+    if (next_x < self->map_max_x) {
+        if (prev_y >= 0) {
+            it = pathfinder_append_if_valid(self, it, next_x, prev_y);
+        }
+        it = pathfinder_append_if_valid(self, it, next_x, node->y);
+        if (next_y < self->map_max_y) {
+            it = pathfinder_append_if_valid(self, it, next_x, next_y);
+        }
+    }
+
+    *it = NULL;
+}
+
+
+static float pathfinder_heuristic_cost_estimate(PathFinder* self, int x1, int y1, int x2, int y2)
+{
+    return self->distance_map[abs(x2 - x1)][abs(y2 - y1)] * self->effective_vs_heuristic_tradeoff;
+}
+
+
+static float pathfinder_effective_cost(PathFinder* self, int x1, int y1, int x2, int y2)
+{
+    return self->distance_map[abs(x2 - x1)][abs(y2 - y1)];
+}
+
+
+static PyObject* pathfinder_find(PathFinder* self, PyObject* args)
+{
+    int x = 0;
+    int y = 0;
+    int start_x = 0;
+    int start_y = 0;
+    int goal_x = 0;
+    int goal_y = 0;
+    Cell* closed_set = NULL;
+    Cell* open_set = NULL;
+
+    if (!PyArg_ParseTuple(args, "iiii", &start_x, &start_y, &goal_x, &goal_y)) {
+        return NULL;
+    }
+
+    if (start_x < 0 || start_x >= self->map_max_x || goal_x < 0 || goal_x >= self->map_max_x ||
+        start_y < 0 || start_y >= self->map_max_y || goal_y < 0 || goal_y >= self->map_max_y) {
+        /* Coordinates out of range */
+        return PyList_New(0);
+    }
+
+    for (x = 0; x < self->map_max_x; ++x) {
+        for (y = 0; y < self->map_max_y; ++y) {
+            cell_init(&self->map[x][y]);
+        }
+    }
+
+    open_set = &self->map[start_x][start_y];
+    open_set->h_score = pathfinder_heuristic_cost_estimate(self, start_x, start_y, goal_x, goal_y);
+    open_set->f_score = open_set->g_score + open_set->h_score;
+
+    while (open_set != NULL) {
+        Cell* current = open_set;
+        Cell* neighbor_nodes[9];
+        Cell** nit = NULL;
+
+        if (current->x == goal_x && current->y == goal_y) {
+            PyObject* path = PyList_New(0);
+            for (; current != NULL; current = current->came_from) {
+                PyList_Insert(path, 0, Py_BuildValue("(ii)", current->x, current->y));
+            }
+            return path;
+        }
+
+        open_set = open_set->next;
+        closed_set = cell_insert_sorted(closed_set, current, cell_pointer_less);
+
+        pathfinder_find_neighbor_nodes(self, current, neighbor_nodes);
+        for (nit = neighbor_nodes; *nit != NULL; ++nit) {
+            Cell* neighbor = *nit;
+            if (!cell_contains_sorted(closed_set, neighbor, cell_pointer_less)) {
+                float tentative_g_score = current->g_score + pathfinder_effective_cost(self, current->x, current->y, neighbor->x, neighbor->y);
+                if (!cell_contains(open_set, neighbor)) {
+                    neighbor->h_score = pathfinder_heuristic_cost_estimate(self, neighbor->x, neighbor->y, goal_x, goal_y);
+                    neighbor->g_score = tentative_g_score;
+                    neighbor->f_score = neighbor->g_score + neighbor->h_score;
+                    neighbor->came_from = current;
+                    open_set = cell_insert_sorted(open_set, neighbor, cell_f_score_less);
+                } else if (tentative_g_score < neighbor->g_score) {
+                    neighbor->g_score = tentative_g_score;
+                    neighbor->f_score = neighbor->g_score + neighbor->h_score;
+                    neighbor->came_from = current;
+                }
+            }
+        }
+    }
+
+    return PyList_New(0);
+}
+
+
+static PyObject* pathfinder_add_wall(PathFinder* self, PyObject* args)
+{
+    Wall* wall = (Wall*) malloc(sizeof(Wall));
+
+    if (!PyArg_ParseTuple(args, "iiii", &wall->x1, &wall->y1, &wall->x2, &wall->y2)) {
+        return NULL;
+    }
+
+    /* Normalize the rectangle */
+    if (wall->x1 > wall->x2) {
+        int tmp = wall->x1;
+        wall->x1 = wall->x2;
+        wall->x2 = tmp;
+    }
+    if (wall->y1 > wall->y2) {
+        int tmp = wall->y1;
+        wall->y1 = wall->y2;
+        wall->y2 = tmp;
+    }
+
+    wall->next = self->walls;
+    self->walls = wall;
+
+    Py_RETURN_NONE;
+}
+
+
+static PyObject* pathfinder_set_main_opponent_position(PathFinder* self, PyObject* args)
+{
+    if (!PyArg_ParseTuple(args, "ii", &self->main_opponent_x, &self->main_opponent_y)) {
+        return NULL;
+    }
+
+    Py_RETURN_NONE;
+}
+
+
+static PyObject* pathfinder_clear_main_opponent_position(PathFinder* self)
+{
+    self->main_opponent_x = -1;
+    self->main_opponent_y = -1;
+
+    Py_RETURN_NONE;
+}
+
+
+static PyObject* pathfinder_set_secondary_opponent_position(PathFinder* self, PyObject* args)
+{
+    if (!PyArg_ParseTuple(args, "ii", &self->secondary_opponent_x, &self->secondary_opponent_y)) {
+        return NULL;
+    }
+
+    Py_RETURN_NONE;
+}
+
+
+static PyObject* pathfinder_clear_secondary_opponent_position(PathFinder* self)
+{
+    self->secondary_opponent_x = -1;
+    self->secondary_opponent_y = -1;
+
+    Py_RETURN_NONE;
+}
+
+
+/****************************/
+/* Python object definition */
+/****************************/
+
+
+/* Object methods */
+static PyMethodDef PathFinder_methods[] = {
+    { "find"    , (PyCFunction) pathfinder_find    , METH_VARARGS, "Find the route from (x1, y1) to (x2, y2)" },
+    { "add_wall", (PyCFunction) pathfinder_add_wall, METH_VARARGS, "Add a wall (x1, y1) to (x2, y2)" },
+    { "set_main_opponent_position", (PyCFunction) pathfinder_set_main_opponent_position, METH_VARARGS, "Sets the main opponent position" },
+    { "clear_main_opponent_position", (PyCFunction) pathfinder_clear_main_opponent_position, METH_NOARGS, "Clears the main opponent position" },
+    { "set_secondary_opponent_position", (PyCFunction) pathfinder_set_secondary_opponent_position, METH_VARARGS, "Sets the secondary opponent position" },
+    { "clear_secondary_opponent_position", (PyCFunction) pathfinder_clear_secondary_opponent_position, METH_NOARGS, "Clears the main opponent position" },
+    {NULL}  /* Sentinel */
+};
+
+
+/* Object members */
+static PyMemberDef PathFinder_members[] = {
+    {NULL}  /* Sentinel */
+};
+
+
+/* Object definition */
+static PyTypeObject PathFinderType = {
+    PyObject_HEAD_INIT(NULL)
+    0,                                        /* ob_size */
+    "pathfinding.PathFinder",                 /* tp_name */
+    sizeof(PathFinder),                       /* tp_basicsize */
+    0,                                        /* tp_itemsize */
+    (destructor) pathfinder_dealloc,          /* tp_dealloc */
+    0,                                        /* tp_print */
+    0,                                        /* tp_getattr */
+    0,                                        /* tp_setattr */
+    0,                                        /* tp_compare */
+    0,                                        /* tp_repr */
+    0,                                        /* tp_as_number */
+    0,                                        /* tp_as_sequence */
+    0,                                        /* tp_as_mapping */
+    0,                                        /* tp_hash */
+    0,                                        /* tp_call */
+    0,                                        /* tp_str */
+    0,                                        /* tp_getattro */
+    0,                                        /* tp_setattro */
+    0,                                        /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
+    "Path finder object",                     /* tp_doc */
+    0,                                        /* tp_traverse */
+    0,                                        /* tp_clear */
+    0,                                        /* tp_richcompare */
+    0,                                        /* tp_weaklistoffset */
+    0,                                        /* tp_iter */
+    0,                                        /* tp_iternext */
+    PathFinder_methods,                       /* tp_methods */
+    PathFinder_members,                       /* tp_members */
+    0,                                        /* tp_getset */
+    0,                                        /* tp_base */
+    0,                                        /* tp_dict */
+    0,                                        /* tp_descr_get */
+    0,                                        /* tp_descr_set */
+    0,                                        /* tp_dictoffset */
+    (initproc) pathfinder_init,               /* tp_init */
+    0,                                        /* tp_alloc */
+    PyType_GenericNew,                        /* tp_new */
+};
+
+
+static PyMethodDef module_methods[] = {
+    {NULL}  /* Sentinel */
+};
+
+
+PyMODINIT_FUNC initpathfinding(void)
+{
+    PyObject* m;
+
+    if (PyType_Ready(&PathFinderType) < 0)
+        return;
+
+    m = Py_InitModule3("pathfinding", module_methods, "Path finding algorithm.");
+
+    if (m == NULL)
+      return;
+
+    Py_INCREF(&PathFinderType);
+    PyModule_AddObject(m, "PathFinder", (PyObject*) &PathFinderType);
+}
--- bhbot/brewery/trajectory.py
+++ bhbot/brewery/trajectory.py
@@ -7,6 +7,7 @@ import bisect
 import tools
 import packets
 import logger
+import pathfinding
 from definitions import *
 
 
@@ -63,14 +64,18 @@ class Map(object):
 
     MAP_WIDTH  = int(FIELD_WIDTH  / MAP_CELL_RESOLUTION)
     MAP_HEIGHT = int(FIELD_HEIGHT / MAP_CELL_RESOLUTION)
-    DISTANCE_MAP = None
     MAP_WALLS_CELLS = 9 # int(MAP_WALLS_DISTANCE / MAP_CELL_RESOLUTION)
-    MAIN_OPPONENT_AVOIDANCE_CELLS = MAIN_OPPONENT_AVOIDANCE_RANGE / MAP_CELL_RESOLUTION
-    SECONDARY_OPPONENT_AVOIDANCE_CELLS = SECONDARY_OPPONENT_AVOIDANCE_RANGE / MAP_CELL_RESOLUTION
+    MAIN_OPPONENT_AVOIDANCE_CELLS = int(MAIN_OPPONENT_AVOIDANCE_RANGE / MAP_CELL_RESOLUTION)
+    SECONDARY_OPPONENT_AVOIDANCE_CELLS = int(SECONDARY_OPPONENT_AVOIDANCE_RANGE / MAP_CELL_RESOLUTION)
+
 
     def __init__(self, eventloop):
         self.eventloop = eventloop
-        self.cells = []
+        self.pathfinder = pathfinding.PathFinder(Map.MAP_HEIGHT,
+                                                 Map.MAP_WIDTH,
+                                                 ASTAR_EFFECTIVE_VS_HEURISTIC_TRADEOFF,
+                                                 Map.MAIN_OPPONENT_AVOIDANCE_CELLS,
+                                                 Map.SECONDARY_OPPONENT_AVOIDANCE_CELLS)
 
         self.walls = []
         self.walls.append([0.5 - MAP_WALLS_DISTANCE, 0.0, 0.52 + MAP_WALLS_DISTANCE, 0.5 + MAP_WALLS_DISTANCE])
@@ -81,38 +86,30 @@ class Map(object):
         for wall in self.walls:
             for i in xrange(len(wall)):
                 wall[i] = int(wall[i] / MAP_CELL_RESOLUTION)
+            self.pathfinder.add_wall(wall[0], wall[1], wall[2], wall[3])
 
 
-        self.main_opponent = None
-        self.secondary_opponent = None
+    def route(self, start_x, start_y, goal_x, goal_y):
+        if IS_HOST_DEVICE_PC:
+            self.eventloop.send_packet(packets.SimulatorResetRoutePath())
 
-        if Map.DISTANCE_MAP == None:
-            Map.DISTANCE_MAP = []
-            for x in xrange(Map.MAP_HEIGHT + 1):
-                row = []
-                x2 = x ** 2
-                for y in xrange(Map.MAP_WIDTH + 1):
-                    row.append(math.sqrt(x2 + y ** 2))
-                Map.DISTANCE_MAP.append(row)
+        logger.log("Compute route from ({}, {}) to ({}, {})".format(start_x, start_y, goal_x, goal_y))
 
+        start_cell_x = int(start_x / MAP_CELL_RESOLUTION)
+        start_cell_y = int(start_y / MAP_CELL_RESOLUTION)
+        goal_cell_x = int(goal_x / MAP_CELL_RESOLUTION)
+        goal_cell_y = int(goal_y / MAP_CELL_RESOLUTION)
 
-    def heuristic_cost_estimate(self, x1, y1, x2, y2):
-        return Map.DISTANCE_MAP[abs(x1 - x2)][abs(y1 - y2)] * ASTAR_EFFECTIVE_VS_HEURISTIC_TRADEOFF
+        path = self.pathfinder.find(start_cell_x, start_cell_y, goal_cell_x, goal_cell_y)
 
-
-    def effective_cost(self, x1, y1, x2, y2):
-        return Map.DISTANCE_MAP[abs(x1 - x2)][abs(y1 - y2)]
-
-
-    def simplified_route(self, start_x, start_y, goal_x, goal_y):
-        path = self.route(start_x, start_y, goal_x, goal_y)
-        logger.log(str(path))
         simplified_path = []
+
         if len(path) > 1:
-            cleaned_path = [ path[0] ]
+            cleaned_path = [ Pose(path[0][0], path[0][1]) ]
             for i in xrange(1, len(path)):
-                if path[i].x != path[i - 1].x and path[i].y != path[i - 1].y:
-                    cleaned_path.append(path[i])
+                if path[i][0] != path[i - 1][0] and path[i][1] != path[i - 1][1]:
+                    (x, y) = path[i]
+                    cleaned_path.append(Pose(x, y))
             p1 = cleaned_path[0]
             p2 = None
             p3 = None
@@ -127,121 +124,14 @@ class Map(object):
                    p1 = p2
             if p3 != None:
                 simplified_path.append(p3)
+
+        logger.log(str(simplified_path))
+
         if IS_HOST_DEVICE_PC:
             self.send_route_to_simulator(path, False)
             self.send_route_to_simulator(simplified_path, True)
+
         return simplified_path
-
-
-    def route(self, start_x, start_y, goal_x, goal_y):
-        if IS_HOST_DEVICE_PC:
-            self.eventloop.send_packet(packets.SimulatorResetRoutePath())
-
-        logger.log("Compute route from ({}, {}) to ({}, {})".format(start_x, start_y, goal_x, goal_y))
-
-        self.cells = {}
-
-        closedset_keys = set()
-        openset = []
-        openset_keys = set()
-        max_openset_size = 1
-
-
-        start_cell_x = int(start_x / MAP_CELL_RESOLUTION)
-        start_cell_y = int(start_y / MAP_CELL_RESOLUTION)
-        goal_cell_x = int(goal_x / MAP_CELL_RESOLUTION)
-        goal_cell_y = int(goal_y / MAP_CELL_RESOLUTION)
-
-        start = MapCell(start_cell_x, start_cell_y)
-        start.g_score = 0
-        start.h_score = self.heuristic_cost_estimate(start_cell_x, start_cell_y, goal_cell_x, goal_cell_y)
-        start.f_score = start.g_score + start.h_score
-
-        openset.append(start)
-        self.cells[start.key] = start
-        openset_keys.add(start.key)
-
-        while len(openset) != 0:
-            current = openset[0]
-            if current.x == goal_cell_x and current.y == goal_cell_y:
-                path = []
-                p = current
-                while p.came_from != None:
-                    path.append(Pose(p.x, p.y))
-                    p = p.came_from
-                logger.log("Route computed. Length: {}. Max openset size: {}".format(len(path), max_openset_size))
-                return path
-
-            del openset[0]
-            openset_keys.remove(current.key)
-            closedset_keys.add(current.key)
-
-            for neighbor in self.neighbor_nodes(current):
-                if not neighbor.key in closedset_keys:
-                    tentative_g_score = current.g_score + self.effective_cost(current.x, current.y, neighbor.x, neighbor.y)
-                    if not neighbor.key in openset_keys:
-                        neighbor.h_score = self.heuristic_cost_estimate(neighbor.x, neighbor.y, goal_cell_x, goal_cell_y)
-                        neighbor.g_score = tentative_g_score
-                        neighbor.f_score = neighbor.g_score + neighbor.h_score
-                        neighbor.came_from = current
-                        bisect.insort_left(openset, neighbor)
-                        openset_keys.add(neighbor.key)
-                        if (len(openset) > max_openset_size):
-                            max_openset_size = len(openset)
-                    elif tentative_g_score < neighbor.g_score:
-                        neighbor.g_score = tentative_g_score
-                        neighbor.f_score = neighbor.g_score + neighbor.h_score
-                        neighbor.came_from = current
-
-        logger.log("No route found")
-        return []
-
-
-    def neighbor_nodes(self, node):
-        n = []
-        prev_x = node.x - 1
-        next_x = node.x + 1
-        prev_y = node.y - 1
-        next_y = node.y + 1
-
-        if prev_x >= 0:
-            if prev_y >= 0:
-                self.append_if_valid(n, prev_x, prev_y)
-            self.append_if_valid(n, prev_x, node.y)
-            if next_y < Map.MAP_WIDTH:
-                self.append_if_valid(n, prev_x, next_y)
-        if prev_y >= 0:
-            self.append_if_valid(n, node.x, prev_y)
-        if next_y < Map.MAP_WIDTH:
-            self.append_if_valid(n, node.x, next_y)
-        if next_x < Map.MAP_HEIGHT:
-            if prev_y >= 0:
-                self.append_if_valid(n, next_x, prev_y)
-            self.append_if_valid(n, next_x, node.y)
-            if next_y < Map.MAP_WIDTH:
-                self.append_if_valid(n, next_x, next_y)
-
-        return n
-
-
-    def append_if_valid(self, container, x, y):
-        if x < Map.MAP_WALLS_CELLS or x > (Map.MAP_HEIGHT - Map.MAP_WALLS_CELLS) or y < Map.MAP_WALLS_CELLS or y > (Map.MAP_WIDTH - Map.MAP_WALLS_CELLS):
-            return
-        if self.main_opponent != None and Map.DISTANCE_MAP[abs(x - self.main_opponent[0])][abs(y - self.main_opponent[1])] < Map.MAIN_OPPONENT_AVOIDANCE_CELLS:
-            return
-        if self.secondary_opponent != None and Map.DISTANCE_MAP[abs(x - self.main_opponent[0])][abs(y - self.main_opponent[1])] < Map.SECONDARY_OPPONENT_AVOIDANCE_CELLS:
-            return
-        for wall in self.walls:
-            in_wall = x >= wall[0] and x <= wall[2] and y >= wall[1] and y <= wall[3]
-            if in_wall:
-                return
-        key = MapCell.make_key(x, y)
-        if self.cells.has_key(key):
-            container.append(self.cells[key])
-        else:
-            node = MapCell(x, y)
-            self.cells[node.key] = node
-            container.append(node)
 
 
     def send_route_to_simulator(self, path, is_simplified):
@@ -254,7 +144,10 @@ class Map(object):
             if len(packet.points) == max_elements:
                 self.eventloop.send_packet(packet)
                 packet.points = []
-            packet.points.append((cell.x, cell.y))
+            if isinstance(cell, Pose):
+                packet.points.append((cell.x, cell.y))
+            else:
+                packet.points.append(cell)
         if len(packet.points) != 0:
             self.eventloop.send_packet(packet)
 
