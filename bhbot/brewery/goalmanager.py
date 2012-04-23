b48c80c0c45729823602f714d4fc71ad4e274371
Yannick Jost <yannick@yjost.com>
2012-04-23 23:56:59 +0200

    Goal manager : new version of goal selection

--- bhbot/brewery/goalmanager.py
+++ bhbot/brewery/goalmanager.py
@@ -2,20 +2,26 @@
 # encoding: utf-8
 
 import sys
+import trajectory
 
 
 
 
 class Goal(object):
 
-    def __init__(self, count, weight, x, y, handler_state):
-        self.count = count
+    def __init__(self, identifier, weight, x, y, handler_state, ctor_parameters = None):
+        self.identifier = identifier
+#        self.count = count
         self.weight = weight
         self.x = x
         self.y = y
         self.handler_state = handler_state
-        self.intrinsic_cost = 0.0
+#        self.intrinsic_cost = 0.0
         self.navigation_cost = 0.0
+#        self.navigation_weight = 0.0
+#        self.normalized_weight = 0.0
+        self.ctor_parameters = ctor_parameters
+        self.score = 0.0
 
 
 
@@ -27,7 +33,6 @@ class GoalManager(object):
         self.harvesting_goals = []
         self.emptying_goals = []
 
-
     def next_goal(self):
         if self.event_loop.robot.tank_full:
             return self.get_best_goal(self.emptying_goals)
@@ -35,37 +40,55 @@ class GoalManager(object):
             return self.get_best_goal(self.harvesting_goals)
 
 
+#    def get_best_goal(self, goals):
+#        #TODO : sort by distance cost, order -> note1, order by weight -> note2 then cost = note1+note2
+#
+#        robot_x = self.event_loop.robot.pose.x
+#        robot_y = self.event_loop.robot.pose.y
+#
+#        # normalize intrinsic cost
+#        intrinsic_max_weight = float((max(goals, key = lambda g: g.weight)).weight)
+#        for goal in goals:
+#            goal.intrinsic_cost = 1.0 - float(goal.weight) / intrinsic_max_weight
+#
+#        # normalize navigation cost
+#        max_navigation_cost = 0
+#        for goal in goals:
+#            pose = trajectory.Pose(goal.x, goal.y, virtual=True)
+#            goal.navigation_cost = self.event_loop.map.evaluate(self.event_loop.robot.pose, pose)
+#            if goal.navigation_cost > max_navigation_cost:
+#                max_navigation_cost = goal.navigation_cost
+#        for goal in goals:
+##            goal.navigation_weight = 1.0 - float(goal.navigation_weight) / float(max_navigation_cost)
+#            goal.navigation_weight = goal.navigation_cost
+#
+#        best_goal = None
+#        best_cost = float(sys.maxint)
+#        for goal in goals:
+#            goal_cost = goal.normalized_weight + goal.navigation_weight
+#            if goal_cost < best_cost:
+#                best_cost = goal_cost
+#                best_goal = goal
+#
+#        return best_goal
+
     def get_best_goal(self, goals):
-        robot_x = self.event_loop.robot.pose.x
-        robot_y = self.event_loop.robot.pose.y
 
-        # normalize intrinsic cost
-        intrinsic_max_weight = float(max(goals, key = lambda g: g.weight))
         for goal in goals:
-            goal.intrinsic_cost = 1.0 - float(goal.weight) / intrinsic_max_weight
+            pose = trajectory.Pose(goal.x, goal.y, virtual=True)
+            goal.navigation_cost = self.event_loop.map.evaluate(self.event_loop.robot.pose, pose)
 
-        # normalize navigation cost
-        max_navigation_cost = 0
-        for goal in goals:
-            goal.navigation_cost = self.event_loop.map.evaluate_cost(robot_x, robot_y, goal.x, goal.y)
-            if goal.navigation_cost > max_navigation_cost:
-                max_navigation_cost = goal.navigation_cost
-        for goal in goals:
-            goal.navigation_weight = 1.0 - float(goal.navigation_weight) / float(max_navigation_cost)
+        for order, goal in enumerate(sorted(goals, key=lambda x:x.navigation_cost)):
+            goal.score = order
 
-        best_goal = None
-        best_cost = float(sys.maxint)
-        for goal in goals:
-            goal_cost = goal.normalized_weight + goal.navigation_weight
-            if goal_cost < best_cost:
-                best_cost = goal_cost
-                best_goal = goal
+        for order, goal in enumerate(sorted(goals, key=lambda x:x.weight, reverse=True)):
+            goal.score += order
+
+        best_goal = max(goals, key=lambda g : g.score)
 
         return best_goal
 
 
     def goal_done(self, goal):
-        if goal in self.harvesting_goals:
-            self.harvesting_goals.remove(goal)
-        if goal in self.emptying_goals:
-            self.emptying_goals.remove(goal)
+        self.harvesting_goals = [ g for g in self.harvesting_goals if g.identifier != goal.identifier ]
+        self.emptying_goals = [ g for g in self.emptying_goals if g.identifier != goal.identifier ]
