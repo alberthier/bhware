16668f9f78480e4ca159f15dfeb0e4d014ce1001
Eric ALBER <eric.alber@gmail.com>
2012-04-29 02:02:39 +0200

    brewery: remove whitespaces from PIC logs

--- bhbot/brewery/eventloop.py
+++ bhbot/brewery/eventloop.py
@@ -133,7 +133,7 @@ class RobotLogDeviceChannel(asyncore.dispatcher):
             while True:
                 i = self.buffer.find("\n")
                 if i != -1:
-                    logger.log(self.buffer[:i], "PIC")
+                    logger.log(self.buffer[:i].rstrip(), "PIC")
                     self.buffer = self.buffer[i + 1:]
                 else:
                     break
