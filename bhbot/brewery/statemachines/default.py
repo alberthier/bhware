68fc7b23fadf7eb11e78ad4b338ceec96147c671
Yannick Jost <yannick@yjost.com>
2012-05-13 12:20:39 +0200

    Commit changes from robot : goal positions, remove tests, rename EmptyTank state to EmptyTreasure

--- bhbot/brewery/statemachines/default.py
+++ bhbot/brewery/statemachines/default.py
@@ -41,10 +41,10 @@ class Main(statemachine.State):
         gm.harvesting_goals.append(goalmanager.Goal("OTHER_SOUTH", 1.0, x2 + offset_x, y2 + offset_y, DIRECTION_BACKWARD, TakeGoldBar))
         gm.harvesting_goals.append(goalmanager.Goal("SWIFFER"    , 0.5, 1.37         , 2.0          , DIRECTION_BACKWARD, Swiffer))
 
-        gm.emptying_goals.append(goalmanager.Goal("DEPOSIT_1", 2.0, 0.25, 0.6, DIRECTION_FORWARD, EmptyTank))
-        gm.emptying_goals.append(goalmanager.Goal("DEPOSIT_2", 1.0, 0.90, 0.5, DIRECTION_FORWARD, EmptyTank))
-        gm.emptying_goals.append(goalmanager.Goal("DEPOSIT_3", 1.0, 0.72, 0.5, DIRECTION_FORWARD, EmptyTank))
-        gm.emptying_goals.append(goalmanager.Goal("DEPOSIT_4", 1.0, 1.06, 0.5, DIRECTION_FORWARD, EmptyTank))
+        gm.emptying_goals.append(goalmanager.Goal("DEPOSIT_1", 2.0, 0.30, 0.6, DIRECTION_FORWARD, DepositTreasure))
+        gm.emptying_goals.append(goalmanager.Goal("DEPOSIT_2", 1.0, 0.90, 0.5, DIRECTION_FORWARD, DepositTreasure))
+        gm.emptying_goals.append(goalmanager.Goal("DEPOSIT_3", 1.0, 0.72, 0.5, DIRECTION_FORWARD, DepositTreasure))
+        gm.emptying_goals.append(goalmanager.Goal("DEPOSIT_4", 1.0, 1.06, 0.5, DIRECTION_FORWARD, DepositTreasure))
 
 
     def on_device_ready(self, packet):
@@ -76,123 +76,9 @@ class WaitStart(statemachine.State):
 class WaitFirstKeepAlive(statemachine.State):
 
     def on_keep_alive(self, packet):
-        #self.switch_to_state(GotoStartPoint())
-        #self.switch_to_state(TestTraj())
-#        self.switch_to_state(TestCommands())
         self.switch_to_state(FindNextGoal())
 
-class MyGoto(statemachine.State):
-    def on_enter(self):
-        walk = commonstates.TrajectoryWalk()
-        walk.backward(2.5)
-        self.switch_to_substate(walk)
 
-
-class TestTraj(statemachine.State):
-
-    def on_enter(self):
-        walk = commonstates.TrajectoryWalk(None, TEAM_UNKNOWN)
-        points = self.event_loop.map.route(self.robot().pose, trajectory.Pose(1.6, 2.0, None, True))
-        x = self.robot().pose.x
-        y = self.robot().pose.y
-        logger.log("{}".format(self.robot().team))
-        for p in points:
-            x = p.x
-            y = p.y
-            walk.look_at_opposite(x, y)
-            walk.move_to(x, y, DIRECTION_BACKWARD)
-        g = packets.Goto()
-        g.direction = DIRECTION_BACKWARD
-        g.points = points
-        #self.send_packet(g)
-        self.switch_to_substate(walk)
-
-
-
-
-class TestCommands(statemachine.State):
-
-    def on_enter(self):
-        seq = commonstates.Sequence()
-
-#        seq.add(commonstates.StoreFabric(FABRIC_STORE_LOW))
-#        seq.add(commonstates.MapArm(MAP_ARM_OPEN))
-#        seq.add(commonstates.MapGripper(MAP_GRIPPER_OPEN))
-#        seq.add(commonstates.MapGripper(MAP_GRIPPER_CLOSE))
-#        seq.add(commonstates.MapArm(MAP_ARM_CLOSE))
-#        seq.add(commonstates.StoreFabric(FABRIC_STORE_HIGH))
-#
-#        seq.add(commonstates.Gripper(GRIPPER_SIDE_LEFT, GRIPPER_OPEN))
-#        seq.add(commonstates.Gripper(GRIPPER_SIDE_RIGHT, GRIPPER_OPEN))
-#        seq.add(commonstates.Gripper(GRIPPER_SIDE_LEFT, GRIPPER_CLOSE))
-#        seq.add(commonstates.Gripper(GRIPPER_SIDE_RIGHT, GRIPPER_CLOSE))
-        seq.add(commonstates.Gripper(GRIPPER_SIDE_BOTH, GRIPPER_OPEN))
-        seq.add(commonstates.Gripper(GRIPPER_SIDE_BOTH, GRIPPER_CLOSE))
-#        seq.add(commonstates.EmptyTank(TANK_DEPLOY))
-#        seq.add(commonstates.EmptyTank(TANK_RETRACT))
-
-#        seq.add(commonstates.Sweeper(SWEEPER_OPEN))
-#        seq.add(commonstates.Sweeper(SWEEPER_CLOSE))
-
-
-        self.switch_to_substate(seq)
-
-
-    def on_exit_substate(self, substate):
-        self.switch_to_state(TestTraj())
-
-
-class GotoStartPoint(statemachine.State):
-
-    def on_enter(self):
-        self.walk = commonstates.TrajectoryWalk()
-        self.walk.move_to(PURPLE_START_X, 0.59)
-        if self.robot().team == TEAM_PURPLE:
-            self.walk.rotate_to(0.0)
-            self.walk.move_to(0.96, 0.59)
-        else:
-            self.walk.rotate_to(math.pi)
-            self.walk.move_to(1.04, 0.59)
-        self.switch_to_substate(self.walk)
-
-
-    def on_exit_substate(self, substate):
-        self.switch_to_state(LoopAroundPeanutIsland())
-
-
-
-
-class LoopAroundPeanutIsland(statemachine.State):
-
-    def on_enter(self):
-        points = [(1.50, 1.06,  math.pi / 2.0),
-                  (1.40, 1.46,  math.pi / 2.0),
-                  (1.50, 1.86,  math.pi / 2.0),
-                  (1.04, 2.41,  math.pi      ),
-                  (0.50, 1.86, -math.pi / 2.0),
-                  (0.60, 1.46, -math.pi / 2.0),
-                  (0.50, 1.06, -math.pi / 2.0),
-                  (0.96, 0.59,  0.0          )]
-
-        if self.robot().team == TEAM_RED:
-            points.reverse()
-            points = points[1:] + points[:1]
-            angle_offset = math.pi
-        else:
-            angle_offset = 0.0
-
-        self.walk = commonstates.TrajectoryWalk()
-
-        for i in xrange(len(points) * 3):
-            k = i % len(points)
-            (x, y, angle) = points[k]
-            self.walk.goto(x, y, angle + angle_offset)
-
-        self.switch_to_substate(self.walk)
-
-
-    def on_exit_substate(self, substate):
-        self.switch_to_state(GotoCaptainRoom())
 
 
 class FindNextGoal(statemachine.State):
@@ -243,6 +129,8 @@ class FindNextGoal(statemachine.State):
                 self.switch_to_state(FindNextGoal())
         else :
             self.switch_to_state(FindNextGoal())
+
+
 
 
 class GrabMap(statemachine.State):
@@ -297,6 +185,40 @@ for k, v in totem_take_positions.items() :
         totem_approach_end_positions[k] = vals
 
 
+
+
+class DepositTreasure(statemachine.State):
+    """ Empty the tank
+    """
+    def __init__(self, goal):
+        self.goal = goal
+
+
+    def on_enter(self):
+        walk = commonstates.TrajectoryWalk()
+        walk.rotate_to(-math.pi / 2.0)
+        walk.forward(0.100)
+        walk.wait_for(commonstates.Gripper(GRIPPER_SIDE_BOTH, GRIPPER_OPEN))
+        walk.wait_for(commonstates.EmptyTank(TANK_DEPLOY))
+        walk.wait_for(commonstates.EmptyTank(TANK_RETRACT))
+        walk.wait_for(commonstates.EmptyTank(TANK_DEPLOY))
+        walk.wait_for(commonstates.EmptyTank(TANK_RETRACT))
+        walk.backward(0.100)
+        walk.wait_for(commonstates.Gripper(GRIPPER_SIDE_BOTH, GRIPPER_CLOSE))
+        self.switch_to_substate(walk)
+
+
+    def on_exit_substate(self, substate):
+        packet = packets.EmptyTankControl()
+        packet.move = TANK_RETRACT
+        self.send_packet(packet)
+        # No need to wait for confirmation here
+        self.robot().goal_manager.goal_done(self.goal)
+        self.exit_substate()
+
+
+
+
 class TakeGoldBar(statemachine.State):
     """ Take a gold bar
     """
@@ -312,7 +234,8 @@ class TakeGoldBar(statemachine.State):
 
     def on_enter(self):
         walk = commonstates.TrajectoryWalk()
-        walk.move_to(*self.start_pos[0:2], direction=DIRECTION_BACKWARD)
+        walk.look_at_opposite(self.start_pos[0], self.start_pos[1])
+        walk.move_to(self.start_pos[0], self.start_pos[1], DIRECTION_BACKWARD)
         walk.rotate_to(self.start_pos[2])
         walk.wait_for(DetectAndTakeGoldbar(self.goal))
         self.switch_to_substate(walk)
@@ -321,29 +244,7 @@ class TakeGoldBar(statemachine.State):
         self.exit_substate()
 
 
-class EmptyTank(statemachine.State):
-    """ Empty the tank
-    """
-    def __init__(self, goal):
-        self.goal = goal
 
-    def on_enter(self):
-        self.switch_to_substate(commonstates.EmptyTank(TANK_DEPLOY))
-
-
-    def on_exit_substate(self, substate):
-        packet = packets.EmptyTankControl()
-        packet.move = TANK_RETRACT
-        self.send_packet(packet)
-        # No need to wait for confirmation here
-        self.robot().goal_manager.goal_done(self.goal)
-        self.exit_substate()
-
-class EndOfMatch(statemachine.State):
-    """End of match"""
-
-    def on_enter(self):
-        pass
 
 class DetectAndTakeGoldbar(statemachine.State):
     def __init__(self, goal):
@@ -377,6 +278,9 @@ class DetectAndTakeGoldbar(statemachine.State):
         self.robot().goal_manager.goal_done(self.goal)
         self.exit_substate()
 
+
+
+
 class Swiffer(statemachine.State):
     """Try to find some pieces to grab"""
     def __init__(self, goal):
@@ -407,3 +311,9 @@ class Swiffer(statemachine.State):
 
 
 
+
+class EndOfMatch(statemachine.State):
+    """End of match"""
+
+    def on_enter(self):
+        pass
--- bhbot/brewery/statemachines/tests.py
+++ bhbot/brewery/statemachines/tests.py
@@ -495,3 +495,115 @@ class TestTakeTreasure(statemachine.State):
     def on_exit_substate(self, substate):
         self.exit_substate()
 
+class MakeRobotsCollide(statemachine.State):
+    def on_enter(self):
+        walk = commonstates.TrajectoryWalk()
+        walk.backward(2.5)
+        self.switch_to_substate(walk)
+
+
+class TestSimpleSquare(statemachine.State):
+
+    def on_enter(self):
+        walk = commonstates.TrajectoryWalk(None, TEAM_UNKNOWN)
+        points = self.event_loop.map.route(self.robot().pose, trajectory.Pose(1.6, 2.0, None, True))
+        x = self.robot().pose.x
+        y = self.robot().pose.y
+        logger.log("{}".format(self.robot().team))
+        for p in points:
+            x = p.x
+            y = p.y
+            walk.look_at_opposite(x, y)
+            walk.move_to(x, y, DIRECTION_BACKWARD)
+        g = packets.Goto()
+        g.direction = DIRECTION_BACKWARD
+        g.points = points
+        #self.send_packet(g)
+        self.switch_to_substate(walk)
+
+
+
+
+class TestCommands(statemachine.State):
+
+    def on_enter(self):
+        seq = commonstates.Sequence()
+
+        #        seq.add(commonstates.StoreFabric(FABRIC_STORE_LOW))
+        #        seq.add(commonstates.MapArm(MAP_ARM_OPEN))
+        #        seq.add(commonstates.MapGripper(MAP_GRIPPER_OPEN))
+        #        seq.add(commonstates.MapGripper(MAP_GRIPPER_CLOSE))
+        #        seq.add(commonstates.MapArm(MAP_ARM_CLOSE))
+        #        seq.add(commonstates.StoreFabric(FABRIC_STORE_HIGH))
+        #
+        #        seq.add(commonstates.Gripper(GRIPPER_SIDE_LEFT, GRIPPER_OPEN))
+        #        seq.add(commonstates.Gripper(GRIPPER_SIDE_RIGHT, GRIPPER_OPEN))
+        #        seq.add(commonstates.Gripper(GRIPPER_SIDE_LEFT, GRIPPER_CLOSE))
+        #        seq.add(commonstates.Gripper(GRIPPER_SIDE_RIGHT, GRIPPER_CLOSE))
+        seq.add(commonstates.Gripper(GRIPPER_SIDE_BOTH, GRIPPER_OPEN))
+        seq.add(commonstates.Gripper(GRIPPER_SIDE_BOTH, GRIPPER_CLOSE))
+        #        seq.add(commonstates.EmptyTank(TANK_DEPLOY))
+        #        seq.add(commonstates.EmptyTank(TANK_RETRACT))
+
+        #        seq.add(commonstates.Sweeper(SWEEPER_OPEN))
+        #        seq.add(commonstates.Sweeper(SWEEPER_CLOSE))
+
+
+        self.switch_to_substate(seq)
+
+
+    def on_exit_substate(self, substate):
+        self.switch_to_state(TestSimpleSquare())
+
+
+class GotoStartPoint(statemachine.State):
+
+    def on_enter(self):
+        self.walk = commonstates.TrajectoryWalk()
+        self.walk.move_to(PURPLE_START_X, 0.59)
+        if self.robot().team == TEAM_PURPLE:
+            self.walk.rotate_to(0.0)
+            self.walk.move_to(0.96, 0.59)
+        else:
+            self.walk.rotate_to(math.pi)
+            self.walk.move_to(1.04, 0.59)
+        self.switch_to_substate(self.walk)
+
+
+    def on_exit_substate(self, substate):
+        self.switch_to_state(LoopAroundPeanutIsland())
+
+
+
+
+class LoopAroundPeanutIsland(statemachine.State):
+
+    def on_enter(self):
+        points = [(1.50, 1.06,  math.pi / 2.0),
+            (1.40, 1.46,  math.pi / 2.0),
+            (1.50, 1.86,  math.pi / 2.0),
+            (1.04, 2.41,  math.pi      ),
+            (0.50, 1.86, -math.pi / 2.0),
+            (0.60, 1.46, -math.pi / 2.0),
+            (0.50, 1.06, -math.pi / 2.0),
+            (0.96, 0.59,  0.0          )]
+
+        if self.robot().team == TEAM_RED:
+            points.reverse()
+            points = points[1:] + points[:1]
+            angle_offset = math.pi
+        else:
+            angle_offset = 0.0
+
+        self.walk = commonstates.TrajectoryWalk()
+
+        for i in xrange(len(points) * 3):
+            k = i % len(points)
+            (x, y, angle) = points[k]
+            self.walk.goto(x, y, angle + angle_offset)
+
+        self.switch_to_substate(self.walk)
+
+
+    def on_exit_substate(self, substate):
+        self.switch_to_state(GotoCaptainRoom())
