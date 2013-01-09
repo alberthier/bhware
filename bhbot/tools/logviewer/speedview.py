c159ed7bfb5374d4ca5dc700ad077d2545037cee
Eric ALBER <eric.alber@gmail.com>
2013-01-10 00:59:36 +0100

    Use matplotlib to display speed graphs

--- bhbot/tools/logviewer/speedview.py
+++ bhbot/tools/logviewer/speedview.py
@@ -12,43 +12,18 @@ from definitions import *
 
 import logtools
 
-
-
-
-class Graph(QGraphicsItemGroup):
-
-    def __init__(self, controller):
-        QGraphicsItemGroup.__init__(self)
-        self.addToGroup(QGraphicsLineItem(self.get_x(0.0),
-                                          self.get_y(-0.1),
-                                          self.get_x(0.0),
-                                          self.get_y(1.1)))
-        lines = 4
-        for i in range(lines + 1):
-            c = i / float(lines)
-            self.addToGroup(QGraphicsLineItem(-10, self.get_y(c), self.get_x(len(controller.speeds)), self.get_y(c)))
-        lines = 10
-        for i in range(lines):
-            c = float((i + 1) * len(controller.speeds)) / float(lines)
-            self.addToGroup(QGraphicsLineItem(self.get_x(c), self.get_y(0.0), self.get_x(c), self.get_y(-0.1)))
-        path = QPainterPath()
-        path.moveTo(self.get_x(0.0), self.get_y(0.0))
-        x = 0
-        for s in controller.speeds:
-            path.lineTo(self.get_x(x), self.get_y(s))
-            x += 1
-        path.lineTo(self.get_x(x - 1), self.get_y(0.0))
-        item = QGraphicsPathItem(path)
-        item.setPen(QPen(QColor("#1B609E"), 2))
-        item.setBrush(QColor("#2279C7"))
-        self.addToGroup(item)
-
-
-    def get_x(self, value):
-        return value * 5.0
-
-    def get_y(self, value):
-        return 100.0 - (value * 100.0)
+matplotlib = None
+try:
+    import matplotlib
+    v = [i for i in map(int, matplotlib.__version__.split('.'))]
+    if not (v[0] >= 1 and v[1] >= 2):
+        del matplotlib
+        matplotlib = None
+    else:
+        from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+        from matplotlib.figure import Figure
+except:
+    print("You need matplotlib v1.2.0 or newer")
 
 
 
@@ -64,34 +39,48 @@ class SpeedViewController(QObject):
 
         self.max_speed = 0.0
         self.average_speed = 0.0
+        self.average_speed_count = 0
 
-        self.scene = QGraphicsScene()
-
-        self.ui.speed_view.setScene(self.scene)
+        if matplotlib is None:
+            label = QLabel(self.ui.speed_tab)
+            label.setText("You need matplotlib v1.2.0 or newer")
+            label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+            self.ui.speed_tab_layout.addWidget(label)
+        else:
+            self.figure = Figure((5.0, 4.0), dpi=100)
+            self.speed_plot = self.figure.add_subplot(111)
+            self.speed_plot.grid(True)
+            self.figure_canvas = FigureCanvas(self.figure)
+            self.figure_canvas.setParent(self.ui.speed_tab)
+            self.ui.speed_tab_layout.addWidget(self.figure_canvas)
 
 
     def process_log_line(self, log_line, lineno, last_lineno):
-        packet_type = log_line[logger.LOG_LINE_PACKET]
-        sender = log_line[logger.LOG_LINE_SENDER]
-        if packet_type is packets.KeepAlive and sender == "PIC":
-            pose = logtools.get_value(log_line[logger.LOG_LINE_CONTENT], "current_pose")
-            x = logtools.get_value(pose, "x")
-            y = logtools.get_value(pose, "y")
-            if len(self.points) > 0:
-                oldx, oldy = self.points[-1]
-                speed = math.sqrt((x - oldx) ** 2 + (y - oldy) ** 2) / (KEEP_ALIVE_DELAY_MS / 1000.0)
-                self.speeds.append(speed)
-                if speed > self.max_speed:
-                    self.max_speed = speed
-                self.average_speed += speed
-            self.points.append((x, y))
-        if lineno == last_lineno:
-            self.average_speed / float(len(self.speeds))
+        if not matplotlib is None:
+            packet_type = log_line[logger.LOG_LINE_PACKET]
+            sender = log_line[logger.LOG_LINE_SENDER]
+            if packet_type is packets.KeepAlive and sender == "PIC":
+                pose = logtools.get_value(log_line[logger.LOG_LINE_CONTENT], "current_pose")
+                x = logtools.get_value(pose, "x")
+                y = logtools.get_value(pose, "y")
+                if len(self.points) > 0:
+                    oldx, oldy = self.points[-1]
+                    speed = math.sqrt((x - oldx) ** 2 + (y - oldy) ** 2) / (KEEP_ALIVE_DELAY_MS / 1000.0)
+                    self.speeds.append(speed)
+                    if speed > self.max_speed:
+                        self.max_speed = speed
+                    if speed > 0.01:
+                        self.average_speed += speed
+                        self.average_speed_count += 1
+                self.points.append((x, y))
+            if lineno == last_lineno:
+                self.average_speed / float(len(self.speeds))
 
 
     def log_loaded(self):
-        self.graph = Graph(self)
-        self.scene.addItem(self.graph)
-        #r = self.graph.boundingRect().normalized()
-        #self.scene.setSceneRect(r)
-        #self.ui.speed_view.fitInView(r, Qt.KeepAspectRatio)
+        if not matplotlib is None:
+            x = [i for i in map(lambda x: x * KEEP_ALIVE_DELAY_MS / 1000.0, range(len(self.speeds)))]
+            self.speed_plot.fill(x, self.speeds, color='#93ABFF', edgecolor='#1E41B9')
+            self.average_speed = self.average_speed / float(self.average_speed_count)
+            self.speed_plot.axhline(self.average_speed, color='g')
+            self.speed_plot.axhline(self.max_speed, color='r')
