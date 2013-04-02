c7d0030979f219b063ef2dc05d8889454a5ee0ff
Eric ALBER <eric.alber@gmail.com>
2013-04-02 22:00:22 +0200

    brewery: write hashfile only once the build is done

--- bhbot/brewery/builder.py
+++ bhbot/brewery/builder.py
@@ -17,45 +17,50 @@ class Builder:
         self.output = output
         self.commands = commands
         self.working_dir = working_dir
+        self.source_path = os.path.join(self.working_dir, self.source)
+        self.output_path = os.path.join(self.working_dir, self.output)
+        self.hfile_path = self.output_path + ".bldh"
 
 
     def is_up_to_date(self):
-        src = os.path.join(self.working_dir, self.source)
-        dst = os.path.join(self.working_dir, self.output)
         sha1 = hashlib.sha1()
         for cmd in self.commands:
             sha1.update(cmd.encode())
-        f = open(src, "rb")
+        f = open(self.source_path, "rb")
         for line in f:
             sha1.update(line)
         f.close()
         newhash = sha1.hexdigest()
 
-        hfile = dst + ".bldh"
-        if os.path.exists(hfile) and os.path.exists(dst):
-            f = open(hfile)
+        if os.path.exists(self.hfile_path) and os.path.exists(self.output_path):
+            f = open(self.hfile_path)
             oldhash = f.readline()
             f.close()
 
             if oldhash == newhash:
-                return True
+                return (True, oldhash)
 
-        f = open(hfile, "w")
-        f.write(newhash)
-        f.close()
-
-        return False
+        return (False, newhash)
 
 
     def build(self):
-        if self.is_up_to_date():
+        (up_to_date, newhash) = self.is_up_to_date()
+        if up_to_date:
             return True
+
+        if os.path.exists(self.output_path):
+            os.remove(self.output_path)
         logger.log(self.commands)
         proc = subprocess.Popen(self.commands, stdout = subprocess.PIPE, stderr = subprocess.PIPE, cwd=self.working_dir)
         (out, err) = proc.communicate()
         if proc.returncode == 0:
             logger.log("'{}' compiled successfully".format(self.source))
         for l in out.splitlines():
-            logger.log(l)
+            logger.log(str(l, "utf-8"))
         for l in err.splitlines():
-            logger.log(l)
+            logger.log(str(l, "utf-8"))
+
+        f = open(self.hfile_path, "w")
+        f.write(newhash)
+        f.close()
+
