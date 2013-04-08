cfa3c8753853cc5a893849895086d2e96e2d1241
Eric ALBER <eric.alber@gmail.com>
2013-04-09 01:42:59 +0200

    drunkstar: add multibyte support for vim and use a better default config
    --HG--
    extra : rebase_source : 599cf7084e1913d24f6c7c6dc9410389ff219e8e

--- drunkstar/postprocess.py
+++ drunkstar/postprocess.py
@@ -8,13 +8,24 @@ from subprocess import *
 
 
 def copy_skeleton():
-    skeleton = os.path.join(os.path.dirname(os.getcwd()), "skeleton")
+    skeleton = os.path.join(os.path.dirname(__file__), "skeleton")
     dest_root = sys.argv[1]
 
     for item in os.listdir(skeleton):
-        call(["cp", "-rf", os.path.join(skeleton, item), dest_root]) 
+        call(["cp", "-rf", os.path.join(skeleton, item), dest_root])
+
+    dotvim = os.path.join(os.path.expanduser("~"), ".vim")
+    call(["cp", "-rf", dotvim, os.path.join(dest_root, "root")])
+    call(["ln", "-s", ".vim/vimrc", os.path.join(dest_root, "root", ".vimrc")])
+
+
+def cleanup():
+    vimdoc = os.path.join(sys.argv[1], "usr", "share", "vim", "vim73", "doc")
+    if os.path.exists(vimdoc):
+        shutil.rmtree(vimdoc)
+
 
 if __name__ == "__main__":
     copy_skeleton()
-    shutil.rmtree(os.path.join(sys.argv[1], "usr", "share", "vim", "vim73", "doc"))
+    cleanup()
     sys.exit(0)
