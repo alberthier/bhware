eadc20dcbf1c88de02d20c54a7567d0f3ff3b048
Eric ALBER <eric.alber@gmail.com>
2013-04-17 02:09:12 +0200

    drunkstar: Fix wifi configuration (at startup and hotplug)
    --HG--
    extra : rebase_source : ec1e5159a718882e6d5ca5e7bbd722d32b794b81

--- drunkstar/skeleton/etc/init.d/start-wifi.sh
+++ drunkstar/skeleton/etc/init.d/start-wifi.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
-for i in 1 2 3 4 5 6 7 8 9 10; do
-    echo "Wifi configuration #$i"
-    iwconfig wlan0 mode ad-hoc essid BHInterbot key 0011223344
-    ip link set wlan0 up
-    if [ `hostname` = "sheldon" ]; then
-        ifconfig wlan0 192.168.3.10
-    elif [ `hostname` = "leonard" ]; then
-        ifconfig wlan0 192.168.3.11
-    else
-        ifconfig wlan0 192.168.3.12
-    fi
-    if ifconfig wlan0; then
+while true; do
+    HAS_WIFI=`iwconfig wlan0 | grep "No such device"`
+    if [ !$HAS_WIFI ]; then
+        iwconfig wlan0 mode ad-hoc essid BHInterbot key 0011223344
+        ip link set wlan0 up
+        if [ `hostname` = "sheldon" ]; then
+            ifconfig wlan0 192.168.3.10
+        elif [ `hostname` = "leonard" ]; then
+            ifconfig wlan0 192.168.3.11
+        else
+            ifconfig wlan0 192.168.3.12
+        fi
         exit 0
-    else
-        sleep 1
     fi
 done
 
