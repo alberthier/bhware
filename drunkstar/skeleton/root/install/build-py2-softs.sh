0763c58c6da48332572657632a1deb4d553be1e9
Éric ALBER <eric.alber@gmail.com>
2014-04-09 01:02:02 +0200

    drunkstar: build python2 stuff on the dockstar itself. strip down the final installation

---
+++ drunkstar/skeleton/root/install/build-py2-softs.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+PYTHON2=Python-2.7.6
+PYTHON2_ARCHIVE=$PYTHON2.tgz
+MERCURIAL=mercurial-2.9.2
+MERCURIAL_ARCHIVE=$MERCURIAL.tar.gz
+
+tar xzf $PYTHON2_ARCHIVE
+cd $PYTHON2
+find . -name "*" -exec touch {} \;
+touch Include/Python-ast.h Python/Python-ast.c
+./configure --enable-shared --disable-ipv6
+make
+make install
+ldconfig
+strip /usr/local/bin/python2.7
+find /usr/local/lib -name "*.so" -exec strip {} \;
+find /usr/local/lib/python2.7 -name *.py -exec rm -f {} ;
+find /usr/local/lib/python2.7 -name *.pyo -exec rm -f {} ;
+rm -rf /usr/local/lib/python2.7/test
+cd ..
+
+tar xzf $MERCURIAL_ARCHIVE
+cd $MERCURIAL
+find . -name "*" -exec touch {} \;
+python setup.py build
+python setup.py install
+ldconfig
+find /usr/local/lib/python2.7/site-packages -name *.py -exec python -m py_compile {} ;
+find /usr/local/lib/python2.7/site-packages -name *.py -exec rm -f {} ;
+find /usr/local/lib/python2.7/site-packages -name *.so -exec strip {} ;
+cd ..
+
