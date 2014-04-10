3cc7b61e4017131238c3907d5afc82e1b0c649f9
Éric ALBER <eric.alber@gmail.com>
2014-04-11 01:07:25 +0200

    Add Python2/3 softs and OpenCV

--- drunkstar/customize.py
+++ drunkstar/customize.py
@@ -6,7 +6,24 @@ from subprocess import *
 
 ROOTFS = "rootfs"
 SKELETON = "skeleton"
-LOCAL_DRUNKSTAR_CORE = "dl/drunkstar-core.tar.bz2"
+DRUNKSTAR_CORE_URL = "https://bitbucket.org/bhteam/bhware-open/downloads/drunkstar_core.tar.bz2"
+LOCAL_DRUNKSTAR_CORE = "dl/drunkstar_core.tar.bz2"
+PYTHON2_SOFTS_URL = "https://bitbucket.org/bhteam/bhware-open/downloads/drunkstar_python-2.7.6_mercurial-2.9.2.tar.bz2"
+LOCAL_PYTHON2_SOFTS = "dl/drunkstar_python-2.7.6_mercurial-2.9.2.tar.bz2"
+PYTHON3_SOFTS_URL = "https://bitbucket.org/bhteam/bhware-open/downloads/drunkstar_python-3.4.0_pyserial-2.7.tar.bz2"
+LOCAL_PYTHON3_SOFTS = "dl/drunkstar_python-3.4.0_pyserial-2.7.tar.bz2"
+OPENCV_URL = "https://bitbucket.org/bhteam/bhware-open/downloads/drunkstar_opencv-2.4.8.tar.bz2"
+LOCAL_OPENCV = "dl/drunkstar_opencv-2.4.8.tar.bz2"
+
+PACKAGES_URLS     = [DRUNKSTAR_CORE_URL,   PYTHON2_SOFTS_URL,   PYTHON3_SOFTS_URL,   OPENCV_URL]
+PACKAGES_ARCHIVES = [LOCAL_DRUNKSTAR_CORE, LOCAL_PYTHON2_SOFTS, LOCAL_PYTHON3_SOFTS, LOCAL_OPENCV]
+
+def download():
+    if not os.path.exists("dl"):
+        os.mkdir("dl")
+    for url, archive in zip(PACKAGES_URLS, PACKAGES_ARCHIVES):
+        if not os.path.exists(archive):
+            call(["wget", "-O", archive, url])
 
 def cleanup():
     if os.path.exists(ROOTFS):
@@ -17,7 +34,8 @@ def cleanup():
 def install_core():
     os.mkdir(ROOTFS)
     print("Extracting Drunkstar Core...")
-    call(["tar", "xjf", LOCAL_DRUNKSTAR_CORE, "-C", ROOTFS])
+    for archive in PACKAGES_ARCHIVES:
+        call(["tar", "xjf", archive, "-C", ROOTFS])
     # remove VIM documentation
     shutil.rmtree(ROOTFS + "/usr/share/vim/vim73/doc")
 
@@ -40,6 +58,7 @@ if __name__ == "__main__":
         print("You must run this program as root")
     else:
         os.chdir(os.path.dirname(__file__))
+        download()
         cleanup()
         install_core()
         install_skeleton()
