e69b5cec9169903a54df69acc677cf590ad2d79b
Éric ALBER <eric.alber@gmail.com>
2014-04-10 23:01:07 +0200

    colordetector: reduce dependencies

--- bhbot/brewery/colordetector/colordetector.cpp
+++ bhbot/brewery/colordetector/colordetector.cpp
@@ -1,5 +1,6 @@
-#include <opencv2/opencv.hpp>
-#include <opencv2/opencv_modules.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 #include <iostream>
 #include <fstream>
