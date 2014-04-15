80cb89f778c7ce6155eb143df2ee86de47894f58
Éric ALBER <eric.alber@gmail.com>
2014-04-16 00:37:18 +0200

    brewery: add remote control commands

--- bhbot/brewery/webinterface.py
+++ bhbot/brewery/webinterface.py
@@ -53,22 +53,62 @@ class WebInterface:
 
     def remotecontrol(self, environ):
         result = {}
-        packets = []
-        packets.append("self.send_packet(EnableAntiBlocking(1))")
-        packets.append("self.send_packet(EnableAntiBlocking(2))")
-        packets.append("self.send_packet(EnableAntiBlocking(3))")
-        packets.append("self.send_packet(EnableAntiBlocking(4))")
-        packets.append("self.send_packet(EnableAntiBlocking(5))")
-        packets.append("self.send_packet(EnableAntiBlocking(6))")
-        result["Packets"] = packets
-        states = []
-        states.append("self.send_packet(EnableAntiBlocking(7))")
-        states.append("self.send_packet(EnableAntiBlocking(8))")
-        states.append("self.send_packet(EnableAntiBlocking(9))")
-        states.append("self.send_packet(EnableAntiBlocking(10))")
-        states.append("self.send_packet(EnableAntiBlocking(11))")
-        states.append("self.send_packet(EnableAntiBlocking(12))")
-        result["States"] = states
+
+        actions = []
+        actions.append("yield GotoHome()")
+        actions.append("yield MoveLineRelative(0.2)")
+        actions.append("yield MoveLineRelative(-0.2, DIRECTION_BACKWARDS)")
+        actions.append("yield RotateRelative(math.radians(45))")
+        actions.append("yield RotateRelative(math.pi / 4.0)")
+        actions.append("yield Trigger(ELEVATOR_UP)")
+        actions.append("yield Trigger(ELEVATOR_DOWN)")
+        actions.append("yield Trigger(ARM_1_OPEN)")
+        actions.append("yield Trigger(ARM_1_CLOSE)")
+        actions.append("yield Trigger(ARM_2_OPEN)")
+        actions.append("yield Trigger(ARM_2_CLOSE)")
+        actions.append("yield Trigger(FIRE_FLIPPER_OPEN)")
+        actions.append("yield Trigger(FIRE_FLIPPER_CLOSE)")
+        actions.append("yield Trigger(TORCH_GUIDE_OPEN)")
+        actions.append("yield Trigger(TORCH_GUIDE_CLOSE)")
+        actions.append("yield Trigger(FRUITMOTH_HATCH_OPEN)")
+        actions.append("yield Trigger(FRUITMOTH_HATCH_CLOSE)")
+        actions.append("yield Trigger(FRUITMOTH_TUB_OPEN)")
+        actions.append("yield Trigger(FRUITMOTH_TUB_CLOSE)")
+        actions.append("yield Trigger(FRUITMOTH_ARM_OPEN)")
+        actions.append("yield Trigger(FRUITMOTH_ARM_CLOSE)")
+        actions.append("yield Trigger(FRUITMOTH_FINGER_OPEN)")
+        actions.append("yield Trigger(FRUITMOTH_FINGER_CLOSE)")
+        actions.append("yield Trigger(GUN_FIRE)")
+        actions.append("yield Trigger(GUN_LOAD)")
+        actions.append("yield Trigger(MAMMOTH_NET_THROW)")
+        actions.append("yield Trigger(MAMMOTH_NET_LOAD)")
+        actions.append("yield Trigger(VALVE_ON)")
+        actions.append("yield Trigger(VALVE_OFF)")
+        actions.append("yield Trigger(PAINT_1_RELEASE)")
+        actions.append("yield Trigger(PAINT_1_HOLD)")
+        actions.append("yield Trigger(PAINT_2_RELEASE)")
+        actions.append("yield Trigger(PAINT_2_HOLD)")
+        actions.append("yield Trigger(PUMP_ON)")
+        actions.append("yield Trigger(PUMP_OFF)")
+        result["Actions"] = actions
+
+        misc = []
+        misc.append("yield Timer(1000)")
+        misc.append("yield StopAll()")
+        misc.append("yield DefinePosition(0.0, 0.0, math.pi / 2.0)")
+        misc.append("yield AntiBlocking(True)")
+        misc.append("yield SpeedControl(0.3)")
+        misc.append("yield Rotate(math.pi / 2.0)")
+        misc.append("yield LookAt(1.0, 1.0)")
+        misc.append("yield LookAtOpposite(1.0, 1.0)")
+        misc.append("yield MoveCurve(math.pi / 2.0, [(0.1, 0.2), (0.4, 0.5)])")
+        misc.append("yield MoveCurve(math.pi / 2.0, [(0.1, 0.2), (0.4, 0.5)], DIRECTION_BACKWARDS)")
+        misc.append("yield MoveLineTo(0.1, 0.0)")
+        misc.append("yield MoveLineTo(0.1, 0.0, DIRECTION_BACKWARDS)")
+        misc.append("yield MoveLine([(0.1, 0.0), (0.2, 0.0), (0.3, 0.0)])")
+        misc.append("yield MoveLine([(0.1, 0.0), (0.2, 0.0), (0.3, 0.0)], DIRECTION_BACKWARDS)")
+        result["Misc"] = misc
+
         return json.dumps(result)
 
 
