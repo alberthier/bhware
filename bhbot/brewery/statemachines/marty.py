370f4d9362372621500f2c712fa519905160a8b8
Éric ALBER <eric.alber@gmail.com>
2014-05-06 00:22:53 +0200

    brewery: Base strategy for Marty

--- bhbot/brewery/statemachines/marty.py
+++ bhbot/brewery/statemachines/marty.py
@@ -24,14 +24,26 @@ class Main(statemachine.State):
         statemachine.StateMachine(self.event_loop, "relaytoggler")
 
         gm = self.robot.goal_manager
-        gm.add(goalmanager.Goal("BorderFireRed1", 1, 2.0 - ROBOT_CENTER_X - 0.03, 1.3, DIRECTION_FORWARD, ToggleFire, None, False, True))
+
+        #                      |       ID       |Weight|                X                    |                     Y                     |     Direction    |     State     | Ctor parameters  |Shared|Navigate|
+        gm.add(goalmanager.Goal("BorderFireW"   ,     1,                                  0.8,                      ROBOT_CENTER_X + 0.03, DIRECTION_FORWARD, PullBorderFire, (-math.pi / 2.0,), False,    True))
+        gm.add(goalmanager.Goal("BorderFireSW"  ,     5, FIELD_X_SIZE - ROBOT_CENTER_X - 0.03,                                        1.3, DIRECTION_FORWARD, PushBorderFire,            (0.0,), False,    True))
+        gm.add(goalmanager.Goal("BorderFireSE"  ,     5,                                  0.8,       FIELD_Y_SIZE - ROBOT_CENTER_X - 0.03, DIRECTION_FORWARD, PullBorderFire,            (0.0,), False,    True))
+        gm.add(goalmanager.Goal("BorderFireE"   ,     5, FIELD_X_SIZE - ROBOT_CENTER_X - 0.03,                         FIELD_Y_SIZE - 1.3, DIRECTION_FORWARD, PushBorderFire,  (math.pi / 2.0,), False,    True))
+        gm.add(goalmanager.Goal("FieldFireW"    ,    10,          1.1 - ROBOT_CENTER_X - 0.03,                                        0.4, DIRECTION_FORWARD, PushFieldFire ,            (0.0,), False,    True))
+        gm.add(goalmanager.Goal("FieldFireW"    ,    10,          1.1 + ROBOT_CENTER_X + 0.03,                                        0.4, DIRECTION_FORWARD, PullFieldFire ,        (math.pi,), False,    True))
+        gm.add(goalmanager.Goal("FieldFireSW"   ,    10,                                  1.6,                0.9 - ROBOT_CENTER_X - 0.03, DIRECTION_FORWARD, PullFieldFire ,  (math.pi / 0.2,), False,    True))
+        gm.add(goalmanager.Goal("FieldFireSW"   ,    10,                                  1.6,                0.9 + ROBOT_CENTER_X + 0.03, DIRECTION_FORWARD, PushFieldFire , (-math.pi / 2.0,), False,    True))
+        gm.add(goalmanager.Goal("FieldFireE"    ,    10,          1.1 - ROBOT_CENTER_X - 0.03,                         FIELD_Y_SIZE - 0.4, DIRECTION_FORWARD, PullFieldFire ,            (0.0,), False,    True))
+        gm.add(goalmanager.Goal("FieldFireE"    ,    10,          1.1 + ROBOT_CENTER_X + 0.03,                         FIELD_Y_SIZE - 0.4, DIRECTION_FORWARD, PushFieldFire ,        (math.pi,), False,    True))
+        gm.add(goalmanager.Goal("FieldFireSE"   ,    10,                                  1.6, FIELD_Y_SIZE - 0.9 - ROBOT_CENTER_X - 0.03, DIRECTION_FORWARD, PullFieldFire ,  (math.pi / 0.2,), False,    True))
+        gm.add(goalmanager.Goal("FieldFireSE"   ,    10,                                  1.6, FIELD_Y_SIZE - 0.9 + ROBOT_CENTER_X + 0.03, DIRECTION_FORWARD, PushFieldFire , (-math.pi / 2.0,), False,    True))
+        gm.add(goalmanager.Goal("HuntTheMammoth",    15,   0.3 + ROBOT_GYRATION_RADIUS + 0.03,                                       2.25, DIRECTION_FORWARD, HuntTheMammoth,              None, False,    True))
 
 
     def on_device_ready(self, packet):
         yield AntiBlocking(True)
-
         yield Trigger(GUN_LOAD, PAINT_1_HOLD, PAINT_2_HOLD, FIRE_FLIPPER_CLOSE)
-
         yield CalibratePosition()
 
 
@@ -41,23 +53,12 @@ class Main(statemachine.State):
         self.send_packet(packets.ServoControl(*FIRE_FLIPPER_OPEN))
         yield MoveLineTo(RED_START_X, 1.2)
         self.send_packet(packets.ServoControl(*FIRE_FLIPPER_CLOSE))
-        yield Rotate(0.0)
+        yield RotateTo(0.0)
         yield MoveLineTo(ROBOT_CENTER_X - 0.02, 1.2)
         yield Trigger(PAINT_1_FLIP_FLOP_START, PAINT_1_FLIP_FLOP_START)
         yield MoveLineTo(0.4, 1.2)
         yield Trigger(PAINT_1_FLIP_FLOP_STOP, PAINT_1_FLIP_FLOP_STOP)
         yield ExecuteGoals()
-
-
-
-
-class ToggleFire(statemachine.State):
-
-    def on_enter(self):
-        yield Trigger(FIRE_FLIPPER_OPEN)
-        yield MoveLineRelative(-0.1, DIRECTION_BACKWARDS)
-        self.exit_reason = GOAL_DONE
-        yield None
 
 
 
@@ -76,6 +77,88 @@ class CalibratePosition(statemachine.State):
             yield MoveLineTo(RED_START_X, 0.0, DIRECTION_BACKWARDS)
             yield SpeedControl()
             yield DefinePosition(None, ROBOT_CENTER_X, math.pi / 2.0)
+        yield None
+
+
+
+
+class HuntTheMammoth(statemachine.State):
+
+    def on_enter(self):
+        yield RotateTo(0.0)
+        yield Trigger(GUN_FIRE)
+        self.exit_reason = GOAL_DONE
+        yield None
+
+
+
+
+class PushBorderFire(statemachine.State):
+
+    def __init__(self, angle):
+        self.angle = angle
+
+
+    def on_enter(self):
+        yield RotateTo(self.angle)
+        yield Trigger(FIRE_FLIPPER_OPEN)
+        yield MoveLineRelative(0.05)
+        yield MoveLineRelative(0.05, DIRECTION_BACKWARDS)
+        yield Trigger(FIRE_FLIPPER_CLOSE)
+        self.exit_reason = GOAL_DONE
+        yield None
+
+
+
+
+class PullBorderFire(statemachine.State):
+
+    def __init__(self, angle):
+        self.angle = angle
+
+
+    def on_enter(self):
+        yield RotateTo(self.angle)
+        yield MoveLineRelative(0.05)
+        yield Trigger(FIRE_FLIPPER_OPEN)
+        yield MoveLineRelative(0.05, DIRECTION_BACKWARDS)
+        yield Trigger(FIRE_FLIPPER_CLOSE)
+        self.exit_reason = GOAL_DONE
+        yield None
+
+
+
+
+class PushFieldFire(statemachine.State):
+
+    def __init__(self, angle):
+        self.angle = angle
+
+
+    def on_enter(self):
+        yield RotateTo(self.angle)
+        yield Trigger(FIRE_FLIPPER_OPEN)
+        yield MoveLineRelative(0.05)
+        yield Trigger(FIRE_FLIPPER_CLOSE)
+        self.exit_reason = GOAL_DONE
+        yield None
+
+
+
+
+class PullFieldFire(statemachine.State):
+
+    def __init__(self, angle):
+        self.angle = angle
+
+
+    def on_enter(self):
+        yield RotateTo(self.angle)
+        yield MoveLineRelative(0.05)
+        yield Trigger(FIRE_FLIPPER_OPEN)
+        yield MoveLineRelative(0.05, DIRECTION_BACKWARDS)
+        yield Trigger(FIRE_FLIPPER_CLOSE)
+        self.exit_reason = GOAL_DONE
         yield None
 
 
