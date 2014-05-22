b63bde1a600b3517f143e63b565f1906cd5feaa9
Éric ALBER <eric.alber@gmail.com>
2014-05-22 21:39:39 +0200

    brewery: add interbot teammate detection

--- bhbot/brewery/statemachines/interbot.py
+++ bhbot/brewery/statemachines/interbot.py
@@ -43,20 +43,19 @@ class Main(Timer):
             self.event_loop.send_packet(packet)
 
 
-#    def on_interbot_position(self, packet):
-#        d = tools.distance(packet.pose.x, packet.pose.y, self.robot.pose.x, self.robot.pose.y)
-#        if d < MAIN_ROBOT_GYRATION_RADIUS + SECONDARY_ROBOT_GYRATION_RADIUS + 0.3:
-#            a = tools.angle_between(packet.pose.x, packet.pose.y, self.robot.pose.x, self.robot.pose.y)
-#            a1 = a
-#            a += self.robot.pose.angle
-#            cos_a = math.cos(a)
-#            self.log("Other robot detected m=({:0.4},{:0.4},{:0.4}) o=({:0.4},{:0.4}) d={} a1={} a={} cosa={}".format(self.robot.pose.x, self.robot.pose.y, self.robot.pose.angle, packet.pose.x, packet.pose.y, d, a1, a, cos_a))
-#            packet = packets.OpponentDetected(x = packet.pose.x, y = packet.pose.y, robot = OPPONENT_ROBOT_TEAMMATE)
-#            if cos_a > 0.8:
-#                packet.direction = DIRECTION_FORWARD
-#                self.send_packet(packet)
-#
-#            elif cos_a < -0.8:
-#                packet.direction = DIRECTION_BACKWARDS
-#                self.send_packet(packet)
+    def on_interbot_position(self, packet):
+        d = tools.distance(packet.pose.x, packet.pose.y, self.robot.pose.x, self.robot.pose.y)
+        if d < MAIN_ROBOT_GYRATION_RADIUS + SECONDARY_ROBOT_GYRATION_RADIUS + 0.5:
+            a = tools.angle_between(0.0, 0.0, packet.pose.x - self.robot.pose.x, packet.pose.y - self.robot.pose.y)
+            a1 = a
+            a -= self.robot.pose.angle
+            cos_a = math.cos(a)
+            self.log("Other robot detected m=({:0.4},{:0.4},{:0.4}) o=({:0.4},{:0.4}) d={} a1={} a={} cosa={}".format(self.robot.pose.x, self.robot.pose.y, self.robot.pose.angle, packet.pose.x, packet.pose.y, d, a1, a, cos_a))
+            packet = packets.OpponentDetected(x = packet.pose.x, y = packet.pose.y, robot = OPPONENT_ROBOT_TEAMMATE)
+            if cos_a > 0.8:
+                packet.direction = DIRECTION_FORWARD
+                self.send_packet(packet)
+            elif cos_a < -0.8:
+                packet.direction = DIRECTION_BACKWARDS
+                self.send_packet(packet)
 
