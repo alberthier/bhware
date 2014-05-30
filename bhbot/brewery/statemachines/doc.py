272cdd22c35bd587eddbc5e5a4cd5feb6c27c9df
Éric ALBER <eric.alber@gmail.com>
2014-05-30 08:18:51 +0200

    doc fixes

--- bhbot/brewery/statemachines/doc.py
+++ bhbot/brewery/statemachines/doc.py
@@ -239,7 +239,7 @@ class Main(statemachine.State):
         ninja_dist = ROBOT_GYRATION_RADIUS + 0.02
         if self.robot.team == TEAM_RED:
             ninja_n = mgm.Goal("NinjaaaaaaaaRedN", 4, 1.45, 0.38, DIRECTION_FORWARD, NinjaaaaaaaaRedN, None, False, True)
-            ninja_s = mgm.Goal("NinjaaaaaaaaRedS", 4, 1.51, 0.65, DIRECTION_FORWARD, NinjaaaaaaaaRedS, None, False, True)
+            ninja_s = mgm.Goal("NinjaaaaaaaaRedS", 4, 1.56, 0.65, DIRECTION_FORWARD, NinjaaaaaaaaRedS, None, False, True)
         else:
             ninja_n = mgm.Goal("NinjaaaaaaaaYellowN", 4, 1.45, sym_y(2.52), DIRECTION_FORWARD, NinjaaaaaaaaYellowN, None, False, True)
             ninja_s = mgm.Goal("NinjaaaaaaaaYellowS", 4, 1.7, sym_y(2.60), DIRECTION_FORWARD, NinjaaaaaaaaYellowS, None, False, True)
@@ -319,7 +319,7 @@ class Main(statemachine.State):
         yield Trigger(TORCH_GUIDE_CLOSE)
 
 #        yield TimerWaitTeamMateToLeave(1000, dy = 0.5)
-        yield Timer(1000)
+        yield Timer(2000)
         yield MoveLineTo(self.start_x, RED_START_Y)
 
         # huntgoal = self.robot.goal_manager.get_goals("HuntTheMammoth")[0]
